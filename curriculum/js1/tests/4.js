011806526f39a466362380603728e3d33da60051
Alberto Lopez <alopez@garagescript.org>
2018-01-16 18:40:09 -0800

    printStr

---
+++ curriculum/js1/tests/4.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+let solution = require('../4').solution;
+
+describe('print string x number of times', () => {
+  it('should repeat string 2 times', () => {
+    const result = solution(2, 'hello');
+    expect(result).to.equal('hellohello');
+  });
+  it('should repeat string 3 times', () => {
+    const result = solution(3, 'hello');
+    expect(result).to.equal('hellohellohello');
+  });
+  it('should repeat string 4 times', () => {
+    const result = solution(4,'hello');
+    expect(result).to.equal('hellohellohellohello');
+  });
+  it('should repeat string 5 times', () => {
+    const result = solution(5, 'hello');
+    expect(result).to.equal('hellohellohellohellohello');
+  });
+})
