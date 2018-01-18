d75ee831cedc64f3f1b3273e48cc802497962a2c
Alberto Lopez <alopez@garagescript.org>
2018-01-17 21:11:59 -0800

    sayHello

--- curriculum/js1/tests/3.js
+++ curriculum/js1/tests/3.js
@@ -2,9 +2,9 @@ const expect = require('chai').expect;
 let solution = require('../3').solution;
 
 describe('repeat string hello x times', () => {
-  it('should repeat string hello 2 times', () => {
-    const result = solution(2);
-    expect(result).to.equal('hellohello');
+  it('should repeat string hello 0 times', () => {
+    const result = solution(0);
+    expect(result).to.equal('');
   });
   it('should repeat string hello 3 times', () => {
     const result = solution(3);
