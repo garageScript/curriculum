dc1d12769241769ddee9943e0a69fefce8f4c4fc
Alberto Lopez <alopez@garagescript.org>
2018-01-22 18:09:36 -0800

    callX2

--- curriculum/js1/tests/5_1.js
+++ curriculum/js1/tests/5_1.js
@@ -1,25 +1,25 @@
 const expect = require('chai').expect;
 const solution = require('../5_1').solution;
 
-describe('call funct x number of timesall funct x number of times with parameter y and return sum', () => {
-  it('should be sum of 3', () => {
+describe('call funct x number of times using parameter y', () => {
+  it('should call function 3 times', () => {
     let counter = 0;
     const result = solution(()=>{counter+=1},3);
-    expect(counter).to.equal(result);
+    expect(counter).to.equal(3);
   });
-  it('should be sum of 5', () => {
+  it('should call function 5 times', () => {
     let counter = 0;
     const result = solution(()=>{counter+=1},5);
-    expect(counter).to.equal(result);
+    expect(counter).to.equal(5);
   });
-  it('should be sum of 20', () => {
+  it('should call function 20 times', () => {
     let counter = 0;
     const result = solution(()=>{counter+=1},20);
-    expect(counter).to.equal(result);
+    expect(counter).to.equal(20);
   });
-  it('should be sum of 30', () => {
+  it('should call function 30 times', () => {
     let counter = 0;
     const result = solution(()=>{counter+=1},30);
-    expect(counter).to.equal(result);
+    expect(counter).to.equal(30);
   });
 });
