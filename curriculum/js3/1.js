9a3e87b3037e47b71a6d4ff6674425025ca8b962
Alberto Lopez <alopez@garagescript.org>
2018-01-25 21:16:51 -0800

    biggestValue

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -1,18 +1,11 @@
 /**
  * biggestValue - takes in object, returns largest value
  * @param {object} obj
- * @param {array} keys
- * @returns {number} 
+ * @returns {number} max
  */
 
-const solution = (obj, keys=Object.keys(obj), i=0, max=obj[keys[i]])=>{
-  if(i===keys.length){
-    return max;
-    }
-  if(obj[keys[i]]>max){
-    max = obj[keys[i]];
-  }
-  return solution(obj, keys, i+1, max);
+const solution = (obj)=>{
+  return 0;
 }
 
 module.exports = {
