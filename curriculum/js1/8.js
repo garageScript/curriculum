4daabfcd8dbdbbbe878afa16d5eb803b617750d4
Madhu <madhusudhan9kings@gmail.com>
2018-01-30 20:14:23 -0800

    solution 8.js

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,14 +5,11 @@
  * @returns {number}
  */
 
-const solution = (a, b, arr=[],i=1) => {
-  if(a>b){
-    if(i>b) return Math.max(...arr);
-  }else{
-    if(i>a) return Math.max(...arr);
-  };
-  if(a%i==0 && b%i==0)arr.push(i)
-return solution(a, b, arr, i+1);
+const solution = (a, b, ans=1,i=1) => {
+  
+    if(i>b || i>a) return ans; 
+  if(a%i==0 && b%i==0)ans = i;
+return solution(a, b, ans, i+1);
 }
 module.exports = {
   solution,
