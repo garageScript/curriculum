2e3dd0975ffeedd441e8cafacf36bd9f187f1ae9
Alberto Lopez <alopez@garagescript.org>
2018-02-05 14:42:47 -0800

    filter

---
+++ curriculum/js3/6.js
@@ -0,0 +1,13 @@
+/*
+ * filter - takes in an object and a function. Return a new object
+ * with key/value if the function returns true. 
+ * Example: 
+ * input: {5:'blah blah',name:'ho', zolo:'4thech'},(k,v)=>{return v.length > 3}
+ * output: {5:'blah blah', zolo:'4thech'}
+*/
+const solution = (a,b)=>{
+  return 0;
+}
+module.exports = {
+  solution
+}
