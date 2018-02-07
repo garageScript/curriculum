6f494dfc3ff9b52b78cee28d500d72156cd79346
V. Lai <lai.vivian@gmail.com>
2018-02-07 15:28:13 -0800

    update some tests

--- curriculum/js2/tests/6.js
+++ curriculum/js2/tests/6.js
@@ -5,7 +5,7 @@ describe('add all elements > 5 to array', () => {
   it('should handle base case of []', () => {
     const result = solution([]);
     expect(result).to.deep.equal([]);
-  }):
+  });
   it('should return [6,7] when input is [6,7]', () => {
     const result = solution([6,7]);
     expect(result).to.deep.equal([6,7]);
