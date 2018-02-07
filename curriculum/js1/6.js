c0fde11cd84397894a499f86d4d1fa87ebf9e9cb
Saumya Singh <saumyasing@gmail.com>
2018-02-07 15:27:35 -0800

    return sum of all factors of num

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,10 +5,17 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (num, sum=0, i=2) => {
+  if(i==num){
+    return sum;
+  }
+  if(num%i==0){
+    sum =sum+i;
+  }
+  return solution(num,sum, i+1);
 };
 
 module.exports = {
   solution,
 };
+
