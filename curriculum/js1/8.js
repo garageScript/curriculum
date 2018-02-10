718d104a4ddb4de53ffd2a18a4774011cac73fce
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-02-10 14:55:09 -0800

    js8 initial commit

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,9 +5,21 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, min = b) => {
+  min = Math.min(a, b, min);
+
+  if (min == 0) {
+    return null;
+  }
+
+  if (a % min == 0 && b % min == 0) {
+    return min;
+  }
+
+  return solution(a, b, min - 1);
 };
+
+console.log(solution(6, 15));
 
 module.exports = {
   solution,
