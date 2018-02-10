cd900621034460c0fb90942970b690fe8984b6db
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-02-10 14:43:44 -0800

    j3 initial commit

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -5,9 +5,20 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b = 'hello') => {
+  if (a == 1) {
+    return b;
+  }
+
+  if (a == 0) {
+    return '';
+  }
+
+  let new_string = b + solution(a - 1, b);
+  return new_string;
 };
+
+console.log(solution(1));
 
 module.exports = {
   solution,
