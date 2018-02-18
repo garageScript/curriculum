84a7399ba31cb40dc694d0f83d0ff8c973fcdfb2
Omar Ihmoda <omarihmoda@DN51v59l.SUNet>
2018-02-18 08:29:54 -0800

    initial commit

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -5,10 +5,31 @@
  * @returns {array}
  */
 
-const solution = (a) => {
-  return [];
+const solution = (a, idx = 0) => {
+  if (idx === a.length) {
+    return a;
+  }
+
+  if (!isPrime(a[idx])) {
+    a[idx] = 1;
+  }
+
+  return solution(a, idx + 1);
+};
+
+const isPrime = (num, val = 2) => {
+  if (val >= num) {
+    return !(num <= 1);
+  }
+
+  if (num % val === 0) {
+    return false;
+  }
+
+  return isPrime(num, val + 1);
 };
 
 module.exports = {
   solution,
+  isPrime,
 };
