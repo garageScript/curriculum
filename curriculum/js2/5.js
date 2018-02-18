f9e364a33ebe98ec7a5c1bb329461f70cc8942af
Omar Ihmoda <omarihmoda@DN51v59l.SUNet>
2018-02-18 08:46:36 -0800

    initial commit

--- curriculum/js2/5.js
+++ curriculum/js2/5.js
@@ -4,8 +4,16 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, idx = 1, largest = a[0]) => {
+  if (idx >= a.length) {
+    return a.length === 0 ? '' : largest;
+  }
+
+  if (a[idx] > largest) {
+    largest = a[idx];
+  }
+
+  return solution(a, idx + 1, largest);
 };
 
 module.exports = {
