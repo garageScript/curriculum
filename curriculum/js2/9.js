1cb711bdd4e30787fcf9901f3685e5f2c795f35d
Omar Ihmoda <omarihmoda@DN51v59l.SUNet>
2018-02-18 09:50:08 -0800

    initial commit

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -4,9 +4,16 @@
  */
 
 const solution = () => {
-  Array.prototype.gsReduce = function () {
-    // To get the actual array, use this
-    return [];
+  Array.prototype.gsReduce = function(callback, accumulator) {
+    return (function gsReduce(arr, callback, idx, accumulator = 0) {
+      if (idx === arr.length) {
+        return accumulator;
+      }
+
+      accumulator = callback(accumulator, arr[idx]);
+
+      return gsReduce(arr, callback, idx + 1, accumulator);
+    })(this, callback, 0, accumulator);
   };
 };
 
