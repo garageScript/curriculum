21412d52731020719feb917c3964f58a9954118a
Omar Ihmoda <omarihmoda@DN51v59l.SUNet>
2018-02-20 10:55:34 -0800

    fixed recursion issue

--- curriculum/js2/8.js
+++ curriculum/js2/8.js
@@ -7,8 +7,15 @@ const solution = () => {
   Array.prototype.gsMap = function(cb, result = []) {
     if (result.length === this.length) return result;
     result.push(cb(this[result.length]));
+    return this.gsMap(cb, result);
   };
 };
+
+solution();
+const myArr = [1, 2, 3];
+
+const result = myArr.gsMap((element) => element * 3);
+console.log(result);
 
 module.exports = {
   solution,
