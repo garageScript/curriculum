0fb4db6a444f3b5a5d5de5db3fc4b4f240d5fdb9
Teju <teju@garagescript.org>
2018-02-20 11:55:55 -0800

    gcd

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -4,11 +4,15 @@
  * @param {number} b
  * @returns {number}
  */
-
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, min = Math.min(a, b)) => {
+    if (min == 0) {
+          return null;
+        }
+    if (a % min == 0 && b % min == 0) {
+          return min;
+        }
+  return solution(a, b, min - 1);
 };
-
 module.exports = {
   solution,
 };
