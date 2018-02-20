26e489c10121e2bd26cab5053e5d48701f73969e
Daniel Thalhuber <danielthalhuber@rover.local>
2018-02-20 13:26:21 -0800

    Add Negative Number Handling for JS1, Exercise 8

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -6,7 +6,7 @@
  */
 
 const solution = (a, b, c = 1, gcd = 1) => {
-  if (c > a || c > b) return gcd;
+  if (c > Math.abs(a) || c > Math.abs(b)) return gcd;
   if (a % c === 0 && b % c === 0) gcd = c;
   return solution(a, b, c + 1, gcd);
 };
