79b332ec795f89b626c8573de3757a81b99c17dc
Omar Ihmoda <omarihmoda@DN51v59l.SUNet>
2018-02-20 10:35:34 -0800

    reset to original

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -4,16 +4,11 @@
  */
 
 const solution = () => {
-  Array.prototype.gsReduce = function(callback, accumulator) {
-    return (function gsReduce(arr, callback, idx, accumulator = 0) {
-      if (idx === arr.length) {
-        return accumulator;
-      }
-
-      accumulator = callback(accumulator, arr[idx]);
-
-      return gsReduce(arr, callback, idx + 1, accumulator);
-    })(this, callback, 0, accumulator);
+  Array.prototype.gsReduce = function(callback, accumulator, idx = 0) {
+    if (idx == this.length) {
+      return accumulator;
+    }
+    return [];
   };
 };
 
