8d329c303ac39c4a71395c7cb6406305d72786a3
Daniel Thalhuber <danielthalhuber@rover.local>
2018-02-19 22:26:08 -0800

    Add solution for JS1, Exercise 7

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,8 +5,10 @@
  * @returns {boolean}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, b = 2) => {
+  if (a < 2 || (a > 2 && a % b === 0)) return false;
+  if (b > Math.sqrt(a)) return true;
+  return solution(a, b + 1);
 };
 
 module.exports = {
