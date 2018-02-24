6670a0fd5623e303ecf2d701839e4924547e4f03
Omar Ihmoda <omarihmoda@DN0a226637.SUNet>
2018-02-24 11:21:08 -0800

    initial commit

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -4,11 +4,27 @@
  * @returns {number} max
  */
 
-const solution = (obj)=>{
-  return 0;
-}
+const solution = (obj, keys = Object.keys(obj), idx = 1, max=obj[keys[0]]) => {
+  let key = keys[idx]
+
+  if(idx === keys.length) return max;
+
+  if(obj[key] > max) max = obj[key];
+
+  return solution(obj, keys, idx+1,max);
+
+};
+
+const my_obj = {
+  first: 100,
+  second: 200,
+  third: 400,
+  fourth: 5000,
+  sixth: 0,
+};
+
+console.log(solution(my_obj));
 
 module.exports = {
-  solution
-}
-
+  solution,
+};
