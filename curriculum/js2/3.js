c91c938a9e1da4628b3b9274ce707bc78330ba6d
kegplant <ace1905@gmail.com>
2018-02-24 10:44:53 -0800

    Solved 2_3

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -6,7 +6,15 @@
  */
 
 const solution = (a) => {
-  return [];
+  return a.map(x => isPrime(x) ? x : 1);
+};
+
+const isPrime = (num, curr = 2) => {
+  num = Math.abs(num);
+  if (num <= 1) return false;
+  if (curr >= num) return true;
+  if (num % curr == 0) return false;
+  return isPrime(num, curr + 1);
 };
 
 module.exports = {
