ff2427203cc0e5bdc9c1ffc19b96af2afe46ec21
David Tae <tae@garagescript.org>
2018-03-03 16:15:08 -0800

    solved js1-#6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,15 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, b=a-1, c=0) => {
+  if (b<=1) {
+    return c;
+  }
+  if ((a%b)===0) {
+    return solution(a, b-1, c+b)
+  }
+  return solution(a,b-1,c)
+ 
 };
 
 module.exports = {
