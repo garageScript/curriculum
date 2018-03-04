560e51bc006d7cb8c960a69ed2ff8a83f0c5f0b7
Song Zheng <song@zheng.club>
2018-03-04 06:53:45 -0800

    fix tests and bugs

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -1,9 +1,9 @@
 /**
- * Max 3 - takes in 3 numbers, return the largest
+ * Takes in 3 numbers, return the largest
  * @param {number} a
  * @param {number} b
  * @param {number} c
- * @returns {boolean}
+ * @returns {number}
  */
 
 const solution = (a, b, c) => {
