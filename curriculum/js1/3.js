b449c14d75d6ef4ea16b24eb8599d531139777cc
Peter Chon <PeteC@garagescript.org>
2018-03-13 20:37:23 -0700

    solves problem 3

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -1,14 +1,15 @@
 /**
+ *
+ *
  * Say Hello - takes in a number x
  *     return string with “hello” repeated x times.
  * @param {number} a
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution= (x) => {
+  return 'hello'.repeat(x);
 };
-
 module.exports = {
   solution,
 };
