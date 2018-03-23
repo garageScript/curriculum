2505350be8338c933718d61bedfb06ec3b18270f
Song Zheng <song@zheng.club>
2018-03-23 15:26:46 -0700

    curriculum tests

---
+++ curriculum/js3/10.js
@@ -0,0 +1,16 @@
+/*
+ * Level BFT - Given a tree, print out an array,
+ * each element is an array of nodes at that level
+ * Each node has a property v, and children which is an array
+ * @param {node} n
+ * @returns {array}
+ */
+
+const solution = (n)=>{
+  return [];
+};
+
+module.exports = {
+  solution,
+};
+
