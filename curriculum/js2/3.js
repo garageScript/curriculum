4f98fb25c4925046f85a24c62033143d4f06f7b4
Carlos <carlos@garagescript.org>
2018-04-15 16:28:03 -0700

    js2 problem 3

--- curriculum/js2/3.js
+++ curriculum/js2/3.js
@@ -5,9 +5,26 @@
  * @returns {array}
  */
 
-const solution = (a) => {
-  return [];
+const solution = (array, i = 0, holder = []) => {
+  const isPrime = (a, c = 2) => {
+    if (a === c) {
+      return true;
+    } else if (a < 2 || a % c === 0) {
+      return false;
+    }
+    return isPrime(a, c + 1);
+  };
+  if (i === array.length) {
+    return holder;
+  }
+  if (isPrime(array[i])) {
+    holder.push(array[i]);
+  } else {
+    holder.push(1);
+  }
+return solution(array, i + 1, holder);
 };
+
 
 module.exports = {
   solution,
