eb234a418ac01ee5e9e5025103b8c441100cd923
Song Zheng <song@zheng.club>
2018-04-21 17:16:39 -0700

    typo changes

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -1,12 +1,12 @@
 /**
  * mostCommon - takes in array, return most common number
  * @param {array} arr
- * @param return {number}
+ * @return {number}
  */
 
 const solution = (arr)=>{
   return 0;
-}
+};
 
 module.exports = {
   solution
