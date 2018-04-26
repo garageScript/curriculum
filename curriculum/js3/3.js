b14946495454a802b20c1b4291716234b9df4f6d
Sophia <sophiaaa@garagescript.org>
2018-04-26 09:53:40 -0700

    partially solve js3 #3: array of duplicates

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -1,13 +1,18 @@
 /*
- * repeatingElements - takes in array of numbers, return an array of all duplicate numbers.
+ * repeatingElements - takes in array of numbers,
+ * return an array of all duplicate numbers.
  * @param {array} a
  * @return {array} b
  */
 
-const solution = (a)=>{
-  return b;
-}
+const solution = (a, i=0, j=i+1, o={}, r=[])=>{
+  if (i === a.length) return r;
+  if (a[i] === a[j] && !r.includes(a[i])) r.push(a[i]);
+  if (j === a.length) return solution(a, i+1, j=i+1, o, r);
+  console.log(r);
+  return solution(a, i, j+1, o, r);
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
