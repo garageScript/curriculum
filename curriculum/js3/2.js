0a2963509887842006667b3741a36886de900711
Apichai Chenthanakij <badchen@garagescript.org>
2018-04-26 23:41:02 -0700

    Solve js3/2.js

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -5,7 +5,29 @@
  */
 
 const solution = (arr)=>{
-  return 0;
+  if (!arr.length) return null;
+
+  let obj = {};  //key -> number, value -> frequency
+  arr.forEach(el => {
+    obj[el] = (obj[el] === undefined) ? 1 : obj[el]+1;
+  });
+  
+  let values = Object.values(obj);
+  let max = 0;
+
+  //get index of highest value
+  let i = values.reduce((acc, el, idx) => {
+    if (el > max) {
+      max = el;
+      return idx;
+    }
+    else {
+      return acc;
+    }
+  },0);
+
+  //key is the number
+  return parseInt(Object.keys(obj)[i]);
 }
 
 module.exports = {
