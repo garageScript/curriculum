ed0cd566f1d97c6941b854d55e928b67a404ef54
Priyanka <pxa4576@garagescript.org>
2018-04-26 20:43:00 -0700

    JS2_5:Return largest number

--- curriculum/js2/5.js
+++ curriculum/js2/5.js
@@ -4,8 +4,11 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (arr, i = 0, largest = arr[i]) => {
+  if(arr.length === 0) return "";
+  if(arr.length === i) return largest;
+  if(arr[i] >= largest) largest = arr[i];
+  return solution(arr, i + 1, largest);
 };
 
 module.exports = {
