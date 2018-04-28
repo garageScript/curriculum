8ec84379871e4fc4b9b65539a7d82492a36b4a09
Craig <craig@garagescript.org>
2018-04-28 15:13:34 -0700

    solution 4

--- curriculum/js1/4.js
+++ curriculum/js1/4.js
@@ -6,8 +6,11 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, result='') => {
+  if (a===0){
+  return result;
+  }
+  return solution(a-1, b, result + b) ;
 };
 
 module.exports = {
