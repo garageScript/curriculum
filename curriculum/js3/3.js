edff8e48a5de941075d9852e6079d0c95c038b4e
Sophia <sophiaaa@garagescript.org>
2018-04-28 18:17:22 -0700

    solve js3 #3 repeatingElements in array

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -5,12 +5,13 @@
  * @return {array} b
  */
 
-const solution = (a, i=0, j=i+1, o={}, r=[])=>{
-  if (i === a.length) return r;
-  if (a[i] === a[j] && !r.includes(a[i])) r.push(a[i]);
-  if (j === a.length) return solution(a, i+1, j=i+1, o, r);
-  console.log(r);
-  return solution(a, i, j+1, o, r);
+const solution = (a, i=0, o={})=>{
+  if (i === a.length) {
+    return Object.keys(o).filter( (k) => o[k] >= 2).map(Number);
+  }
+  if (i < a.length && !o[a[i]]) o[a[i]] = 0;
+  o[a[i]] += 1;
+  return solution(a, i+1, o);
 };
 
 module.exports = {
