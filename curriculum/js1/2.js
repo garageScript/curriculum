f9383d99a47a7955fc9956ef7b1479c64a8a207d
Saumya Singh <saumyasing@gmail.com>
2018-05-23 14:50:47 -0700

    max without Math.max

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -7,7 +7,22 @@
  */
 
 const solution = (a, b, c) => {
- return Math.max(a,b,c);
+  if (a>b){
+    if(b>c){
+      return a;
+    }
+    if(b<c){
+      return c;
+    }
+  }
+  if(a<b){
+    if(b<c){
+      return c;
+    }
+    if(b>c){
+      return b;
+    }
+  }
 };
 
 module.exports = {
