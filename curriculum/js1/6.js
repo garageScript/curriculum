a70308bf7c6ce0817b751b1b4a9f6c5a114fa383
Daniel Thalhuber <danielthalhuber@rover.local>
2018-05-23 19:28:27 -0700

    Refactor Solution

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -6,7 +6,7 @@
  */
 
 const solution = (a, b = 2, sum = 0) => {
-  if (b === a - 2) return sum;
+  if (a < 2 || b === a) return sum;
   if (a % b === 0) sum += b;
   return solution(a, b + 1, sum);
 };
