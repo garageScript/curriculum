1deb036b9cddd7a1a63c7c9b6834a6ddad861108
Russell luttrell <rluttrell@garagescript.org>
2018-05-26 16:38:14 -0700

    js1 2

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -7,7 +7,20 @@
  */
 
 const solution = (a, b, c) => {
-  return 0;
+  if (a > b){
+    if(a > c){
+      return a;
+    }
+    else{
+      return c
+    }
+  }
+  else if(c > b){
+    return c;
+  }
+  else{
+    return b;
+  }
 };
 
 module.exports = {
