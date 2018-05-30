352a7dcad60999b26de4f16da4080d293d67b467
Russell Luttrell <russluttrell@garagescript.org>
2018-05-29 17:53:58 -0700

    js1 6.js

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,12 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, b = a -1,c = 0) => {
+
+  if (b <= 1) { return c;}
+  if ( a % b === 0 )
+    c += b;
+  return solution(a,b - 1, c);
 };
 
 module.exports = {
