10bd06074c2d667f5379ccdcfa1add24b2757432
Russell Luttrell <russluttrell@garagescript.org>
2018-06-02 15:19:43 -0700

    js2-6

--- curriculum/js2/6.js
+++ curriculum/js2/6.js
@@ -4,8 +4,12 @@
  * @returns {array}
  */
 
-const solution = (a) => {
-  return [];
+const solution = (a,i = 0,i2 = 0, ans = []) => {
+  if (a.length === i) return ans;
+  if (a[i] > 5) {
+    ans[i2] = a[i];
+    i2 +=1;}
+  return solution(a,i+1,i2,ans);
 };
 
 module.exports = {
