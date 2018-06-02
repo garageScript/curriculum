5eb4468615c5efb5f017ce1226b4db9d80651bd8
Song Zheng <song@zheng.club>
2018-06-02 14:07:34 -0700

    fix test for problem 2

--- curriculum/js2/tests/5.js
+++ curriculum/js2/tests/5.js
@@ -2,10 +2,6 @@ const expect = require('chai').expect;
 const solution = require('../5').solution;
 
 describe('given an array, return the largest element', () => {
-  it('should handle base case of []', () => {
-    const result = solution([]);
-    expect(result).to.equal('');
-  });
   it('should return 7 when input is [6,7]', () => {
     const result = solution([6,7]);
     expect(result).to.equal(7);
