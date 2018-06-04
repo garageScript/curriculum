e3892a4ba135b1bd6d80a4f34e9747e7d88f1bd7
Russell Luttrell <russluttrell@garagescript.org>
2018-06-04 15:01:46 -0700

    js3-3

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -4,8 +4,17 @@
  * @return {array} b
  */
 
-const solution = (a)=>{
-  return b;
+const solution = (a,b = [],existing = {},i = 0,bi = 0)=>{
+  if(i === a.length) return b;
+  if(!(a[i] in existing)) existing[a[i]] = 1;
+  else{ 
+    existing[a[i]] += 1;
+    if(existing[a[i]] === 2){
+      b[bi] = a[i];
+      bi += 1;
+    }
+  }
+  return solution(a,b,existing,i+1,bi);
 }
 
 module.exports = {
