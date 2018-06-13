076a05b2f5c508f499cd2ae85146dfdd06865a34
Daniel Thalhuber <32470229+danielthalhuber@users.noreply.github.com>
2018-06-13 09:17:33 -0700

    js3/tests/7.js: update test
    * The description for [js3/7.js](https://github.com/garageScript/curriculum/blob/master/curriculum/js3/7.js#L3) states that the solution should return the greatest number (value) in the linked list.
    * This test expected the return value to be the node in the linked list with the largest number value
    * The test script in this PR/MR is aligned with the exercise description

--- curriculum/js3/tests/7.js
+++ curriculum/js3/tests/7.js
@@ -1,46 +1,38 @@
 const expect = require('chai').expect;
 const solution = require('../7').solution;
 
-describe('filter an object if the condition is true', () => {
-   it('should return base case when there is only 1 node', () =>{
-    const obja = {v: 5};
-
-    const result = solution(obja, 5);
-    expect(result).to.deep.equal(obja);
+describe('return the greatest value in the given linked list', () => {
+   it('should return the value of the first node when there is only 1 node', () =>{
+    const obja = { v: 5 };
+    expect(solution(obja)).to.equal(obja.v);
   });
 
-  it('should return the last node', () =>{
-    const obj1 = {v: 1};
-    const obj2 = {v: 2};
+  it('should return the value of the last node', () =>{
+    const obj1 = { v: 1 };
+    const obj2 = { v: 2 };
     obj1.next = obj2;
-
-    const result = solution(obj1, 2);
-    expect(result).to.deep.equal(obj2);
+    expect(solution(obj1)).to.equal(obj2.v);
   });
 
-  it('should return middle node', () =>{
-    const obj1 = {v: 1};
-    const obj2 = {v: 2};
-    const obj3 = {v: 3};
+  it('should return the value of the middle node', () =>{
+    const obj1 = { v: 1 };
+    const obj2 = { v: 3 };
+    const obj3 = { v: 2 };
     obj1.next = obj2;
     obj2.next = obj3;
-
-    const result = solution(obj1, 2);
-    expect(result).to.deep.equal(obj2);
+    expect(solution(obj1)).to.equal(obj2.v);
   });
 
-  it('should return second to last node', () =>{
-    const obj1 = {v: 1};
-    const obj2 = {v: 2};
-    const obj3 = {v: 3};
-    const obj4 = {v: 4};
-    const obj5 = {v: 5};
+  it('should return the value of the second to last node', () =>{
+    const obj1 = { v: 1 };
+    const obj2 = { v: 2 };
+    const obj3 = { v: 3 };
+    const obj4 = { v: 5 };
+    const obj5 = { v: 4 };
     obj1.next = obj2;
     obj2.next = obj3;
     obj3.next = obj4;
     obj4.next = obj5;
-
-    const result = solution(obj1, 4);
-    expect(result).to.deep.equal(obj4);
+    expect(solution(obj1)).to.equal(obj4.v);
   });
 });
