27ecb8a03cdf698275ab5d490381e7763f01c331
hailey liang <haileyliang@garagescript.org>
2018-06-14 17:57:52 -0700

    hljs106

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -1,12 +1,13 @@
-/**
- * getDivisors - takes in a number and returns sum
+/* getDivisors - takes in a number and returns sum
  *   of all the divisors (except 1 or itself)
  * @param {number} a
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, sum=0, i=2) => {
+  if (a===i) return sum;
+  if (a%i===0) sum =sum + i;
+  return solution(a, sum, i+1);
 };
 
 module.exports = {
