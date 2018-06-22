116e1b8ce4f0fd8b299f76cb4ce68f54dfdf5ae5
Song Zheng <song@zheng.club>
2018-06-22 09:47:02 -0700

    3

--- curriculum/end2end/3.js
+++ curriculum/end2end/3.js
@@ -1,5 +1,8 @@
-// Getting To Know Your Computer, pt. 1 - All Except You!
-// Write a script that console.logs all the users inside the /home folder that is not you. Use fs.readdir and read /home directory.
+// Creating Your First Site - Express Yourself
+// Create an express server that sends back <h1>, <input>, and <button> tag. When you visit url/hello. Make the h1 tag red using (CSS).
 
-// const fs = require('fs');
+// const express = require('express');
+// const app = express();
+// app.get('url', handler);
+// app.listen(PORT)
 
