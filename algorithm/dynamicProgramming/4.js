f0162e44d04fce34655cd4c85129a10dbe78784c
Song Zheng <song@zheng.club>
2018-07-22 16:08:56 -0700

    dp 4

---
+++ algorithm/dynamicProgramming/4.js
@@ -0,0 +1,15 @@
+/**
+ * Given an array of weights and a maximum capacity, find the maximum
+ * weight we can achieve.
+ * @param {array} a
+ * @param {number} v
+ * @returns {number}
+ */
+
+const solution = (a, v) => {
+  return 0;
+};
+
+module.exports = {
+  solution,
+};
---
+++ algorithm/dynamicProgramming/tests/4.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const solution = require('../4').solution;
+
+describe('max value', () => {
+  it('should return value for one weight', () => {
+    const result = solution([1], 40);
+    expect(result).to.equal(40);
+  });
+  it('should return value for one weight', () => {
+    const result = solution([30], 40);
+    expect(result).to.equal(30);
+  });
+  it('should return max value for 102', () => {
+    const result = solution([100, 5], 102);
+    expect(result).to.equal(100);
+  });
+  it('should work for 8 value using 1, 4, 5', () => {
+    const result = solution([4, 5], 9);
+    expect(result).to.equal(9);
+  });
+  it('should work for 18 value using 8, 15', () => {
+    const result = solution([8, 15], 18);
+    expect(result).to.equal(16);
+  });
+});
