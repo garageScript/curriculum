be079fa0aad643225a1dd961d418b38f9c890097
Royce Puzon <royce.puzon@berkeley.edu>
2018-08-06 10:46:40 -0700

    Adds DP problem 5

---
+++ algorithm/dynamicProgramming/5.js
@@ -0,0 +1,18 @@
+/**
+ * Given a matrix that represents an map,
+ * return the number of isolated islands in the map.
+ * The map is represented with 1 for land and 0 for water.
+ * With every value in the matrix as a tile,
+ * a land tile is adjacent if the value top, bottom, left, or right
+ * is also a land tile
+ * @param {array} map
+ * @returns {number} of isolated islands
+ */
+
+const solution = (map) => {
+  return 0;
+};
+
+module.exports = {
+  solution,
+};
