77686bedd4d60bc3a61261273f29f390225a7385
Royce Puzon <royce.puzon@berkeley.edu>
2018-08-06 16:14:58 -0700

    Adds missing k parameter for subarray sum equals k

---
+++ algorithm/dynamicProgramming/7.js
@@ -0,0 +1,24 @@
+/*
+ * Given an array of integers and a target value,
+ * return true or false whether or not a contiguous (side-by-side) sequence of
+ * values can sum to the target value
+ *
+ * In other words, is there a subarray of integers that create the target value
+ *
+ * Note that an empty array is not considered a subarray.
+ *
+ * Input: [1, 3, 4, 6, 8], 10
+ * Output: True
+ * Explanation: 4, 6 are adjacent and result to 10
+ *
+ * Input: [1], 0
+ * Output: False
+ */
+
+const solution = (integers, k) => {
+  return true;
+};
+
+module.exports = {
+  solution,
+};
