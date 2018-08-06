016cf517a56dc29f2d3145a245c601d322dd92f2
Royce Puzon <royce.puzon@berkeley.edu>
2018-08-06 15:38:12 -0700

    Adds the 6th problem, minimum delete distance.

---
+++ algorithm/dynamicProgramming/6.js
@@ -0,0 +1,32 @@
+/*
+ * Triplebyte 2:
+ * Deletion distance between 2 strings is minimum sum of ASCII values
+ * of characters that you need to delete in the 2 strings
+ * in order to have the same string.
+ *
+ * Write a function to find deletion distance between 2 strings
+ *
+ * Examples:
+ * Input: 'cat', 'at'
+ * Output: 99
+ * Explanation: delete 'c' (ASCII value 99)
+ *
+ * Input: 'cat', 'bat'
+ * Output: 197
+ * Explanation: delete 'c' and 'b' (ASCII value 99 + 98)
+ *
+ * Input: 'boat', 'got'
+ * Output: 298
+ *
+ * Input: 'thought', 'sloughs'
+ * Output: 674
+ *
+ */
+
+const solution = (wait, onLast, onFirst, interval, timestamps) => {
+  return 0;
+};
+
+module.exports = {
+  solution,
+};
