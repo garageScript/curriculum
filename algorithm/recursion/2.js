e5d28c29a83c17516fcb168e96e96f8f633db563
Song Zheng <song@zheng.club>
2018-08-09 23:43:54 -0700

    root

---
+++ algorithm/recursion/2.js
@@ -0,0 +1,12 @@
+/**
+ * Given a node {val: 5}, return value of the sum of values of biggest subtree
+ * @param {node} a
+ * @returns {number}
+ */
+
+const solution = (n, a, b) => {
+};
+
+module.exports = {
+  solution,
+};
