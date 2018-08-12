502ddea114b6564621f9d62cf6bc8b1a52ae6edb
root <root@garagescript.org>
2018-08-12 14:36:38 -0700

    add color to problem

--- practiceProblems/2.js
+++ practiceProblems/2.js
@@ -1,4 +1,5 @@
 /**
+ * FB contract
  * Write a function that counts the number of islands in a matrix.
  * 1 represent land, 0 represent sea.
  * Land elements are connected if they are adjacent up, down, left, right
