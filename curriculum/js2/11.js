3497ae108f5c09670c9743eca55f1f592e44e929
root <root@garagescript.org>
2018-08-14 16:45:33 -0700

    add test 11

---
+++ curriculum/js2/11.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array, redistribute values greater than 1 to previous elements
+ * that are 0 and change it to 1. All remainders will accumulate at index 0.
+ * @param {array} a
+ * @returns {array}
+ */
+
+const solution = (a) => {
+  return [];
+};
+
+module.exports = {
+  solution,
+};
