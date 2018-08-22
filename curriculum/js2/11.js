c4c6bd74454a9ba8122abacbc03a41529ca79041
Maricris <seemcat@users.noreply.github.com>
2018-08-21 18:19:18 -0700

    Created 11.js

---
+++ curriculum/js2/11.js
@@ -0,0 +1,16 @@
+/**                                                                                                                                                                   
+* Given an array, redistribute                                                                                                                                       
+* values greater than 1 to previous                                                                                                                                  
+* elements that are 0 and change it                                                                                                                                  
+* to 1. All remainders will accumulate                                                                                                                               
+* at index 0.                                                                                                                                                        
+* Example: solution([0,0,3]) ~ returns [1,1,1]                                                                                                                      
+*/                                                                                                                                                                   
+                                                                                                                                                                        
+const solution = (a) => {                                                                                                                                             
+return 0;                                                                                                                                                          
+};                                                                                                                                                                  
+                                                                                                                                                                 
+module.exports = {                                                                                                                                                    
+solution,                                                                                                                                                           
+};
