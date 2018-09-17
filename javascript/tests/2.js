bd660b62edf90207a40c3019910c1d81919a6c53
root <root@garagescript.org>
2018-09-16 17:57:57 -0700

    add throttle funciton

--- javascript/tests/2.js
+++ javascript/tests/2.js
@@ -1,14 +1,15 @@
 const debounce = require('../2.js');
 
-describe('Debounce', function(){
-  it('Debounce should not be called before set time (10ms)', function(done){
+describe('Debounce', function() {
+  it('Debounce should not be called before set time (10ms)', function(done) {
     let counter = 0;
     let z = debounce(() => counter += 1, 10);
     z();
     setTimeout(() => z(), 9);
     setTimeout(() => {
-      if (counter)
-        return done(new Error('Debounced function is called too early'));
+      if (counter) {
+return done(new Error('Debounced function is called too early'));
+}
     }, 18);
     setTimeout(() => {
       if (counter) return done();
@@ -16,15 +17,16 @@ describe('Debounce', function(){
     }, 20);
   });
 
-  it('Debounce should not be called before set time (10ms) even with multiple calls', function(done){
+  it('Debounce should not be called before set time (10ms) even with multiple calls', function(done) {
     let counter = 0;
     let z = debounce(() => counter += 1, 10);
     z();
     z();
     setTimeout(() => z(), 9);
     setTimeout(() => {
-      if (counter)
-        return done(new Error('Debounced function is called too early'));
+      if (counter) {
+return done(new Error('Debounced function is called too early'));
+}
     }, 18);
     setTimeout(() => {
       if (counter) return done();
@@ -32,18 +34,19 @@ describe('Debounce', function(){
     }, 20);
   });
 
-  it('Debounce should not be called while there are setTimeouts < 100ms', function(done){
+  it('Debounce should not be called while there are setTimeouts < 100ms', function(done) {
     const xTimeouts = (x, cb, time, i=0) => {
-      if(i === x) return;
+      if (i === x) return;
       cb();
-      setTimeout(() => xTimeouts(x, cb, i+1, time),time);
+      setTimeout(() => xTimeouts(x, cb, i+1, time), time);
     };
     let counter = 0;
     let z = debounce(() => counter += 1, 10);
     xTimeouts(3, () => z(), 8);
     setTimeout(() => {
-      if(counter)
-        return done(new Error('Debounced function is called when it should not be'));
+      if (counter) {
+return done(new Error('Debounced function is called when it should not be'));
+}
     }, 29);
     setTimeout(() => {
       if (counter === 1) return done();
@@ -51,17 +54,17 @@ describe('Debounce', function(){
     }, 33);
   });
 
-  it('Debounce should be called 100ms after the last function call', function(done){
+  it('Debounce should be called 100ms after the last function call', function(done) {
     const xTimeouts = (x, cb, time, i=0) => {
-      if(i === x) return;
+      if (i === x) return;
       cb();
-      setTimeout(() => xTimeouts(x, cb, i+1, time),time);
+      setTimeout(() => xTimeouts(x, cb, i+1, time), time);
     };
     let counter = 0;
     let z = debounce(() => counter += 1, 100);
     xTimeouts(3, () => z(), 80);
     setTimeout(() => {
-      if(counter === 1) return done();
+      if (counter === 1) return done();
       return done(new Error('Debounced function is either not called or called too many times'));
     }, 590);
   });
