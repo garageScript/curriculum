ad977b7b2392d4d316c91150f7d37030273798c3
Song Zheng <song@zheng.club>
2018-10-08 23:07:00 -0700

    add files and package

--- reactgraphsocket/src/files.js
+++ reactgraphsocket/src/files.js
@@ -1,54 +1,57 @@
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query, Mutation } from "react-apollo";
+import gql from 'graphql-tag';
 import React from 'react';
 import Stars from './stars';
-// Helper function so I don't have to call fetch over and over again
-const post = (query) => {
-  return fetch('/graphql', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-    body: JSON.stringify({query})
-  }).then(r=>r.json())
-};
-const File = ({info}) => {
-  const sendData = (value) => {
-    post(`
-      mutation {
-        rateFile(file: "${info.name}", value: ${value}) {
-            name
-          }
-      }
-    `);
-  };
-  const initialValue = info.value > -1 ? info.value : -1;
-  return (
-    <div>
-      <h3>
-        {info.name}
-      </h3>
-      <Stars select={sendData} value={initialValue} locked={initialValue > -1}/>
-      <hr />
-    </div>
-  );
-};
-class Files extends React.Component {
-  constructor(props) {
-    super(props); // You must call super whenever you extend
-    this.state = {files: []};
-    post(` {files{name,value}} `).then( ({data}) => {
-      this.setState({files: data.files});
-    });
+
+const FILES_QUERY = gql`{files{name,value}}`;
+const RATE_QUERY = gql`
+  mutation RateFile($name: String, $value: Int) {
+    rateFile(file: $name, value: $value) {
+      name
+    }
   }
-  render() {
-    const files = this.state.files.map( (f, i) => {
-      return <File info={f} key={i} />
-    });
-    return (
-      <div>
-        {files}
-      </div>
-    );
-  }
-};
+`;
+
+const client = new ApolloClient({uri: "/graphql"});
+
+const File = ({info}) => (
+  <Mutation mutation={RATE_QUERY}>
+    {(rateFile, {data}) => {
+      const sendData = (value) => {
+        rateFile({variables: {name: info.name, value}});
+      };
+      const initialValue = info.value > -1 ? info.value : -1;
+      return (
+        <div>
+          <h3>
+            {info.name}
+          </h3>
+          <Stars select={sendData} value={initialValue} locked={initialValue > -1}/>
+          <hr />
+        </div>
+      );
+    }}
+  </Mutation>
+);
+
+const Files = () => (
+  <ApolloProvider client={client}>
+    <Query query={FILES_QUERY}>
+      {({loading, error, data}) => {
+        if (loading) return <h3>Loading...</h3>;
+        if (error) return <h3>Error</h3>;
+        const files = data.files.map( (f, i) => {
+          return <File info={f} key={i} />
+        });
+        return (
+          <div>
+            {files}
+          </div>
+        )
+      }}
+    </Query>
+  </ApolloProvider>
+);
+
 export default Files;
