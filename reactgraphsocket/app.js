9aa47f8eb3938a94d2756d10b4e1501d236d6c8f
Song Zheng <song@zheng.club>
2018-10-08 22:36:51 -0700

    two

--- reactgraphsocket/app.js
+++ reactgraphsocket/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const fs = require('fs');
 const {ApolloServer, gql} = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
+
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded());
+
+const messages = [];
 
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       users: [String]
+      messages: [Message]
+    }
+    type Message {
+      name: String
+      value: String
+    }
+    type Mutation {
+      addMessage(value: String): Message
     }
   `,
   resolvers: {
@@ -18,12 +34,29 @@ const server = new ApolloServer({
           });
         });
       },
+      messages: () => messages,
+    },
+    Mutation: {
+      addMessage: (_, args, context) => {
+        const msg = {name: context.req.cookies.name, value: args.value};
+        messages.unshift(msg);
+      },
     },
   },
+  context: ({req}) => ({
+    req,
+  }),
 });
 server.applyMiddleware({app});
 
-app.use(express.static('public'));
+app.get('/chat', (req, res) => {
+  if (!req.cookies.name) return res.redirect('/login.html');
+  return res.sendFile(`${__dirname}/views/chat.html`);
+});
+app.post('/login', (req, res) => {
+  res.cookie('name', req.body.name);
+  res.redirect('/chat');
+});
 
 app.listen(6899); // song.c0d3.com
 
