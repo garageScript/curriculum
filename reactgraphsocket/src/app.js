d802b42cc655d2e8c44ef7ddd1732aadfe2cd759
Song Zheng <song@zheng.club>
2018-10-08 22:56:40 -0700

    five

--- reactgraphsocket/src/app.js
+++ reactgraphsocket/src/app.js
@@ -1,8 +1,14 @@
 import Stars from './stars';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Kanban from './kanban';
+
+const App = () => {
+  if (window.location.pathname.includes('star')) return <Stars />;
+  return <Kanban />
+};
 
 ReactDOM.render(
-  <Stars />,
+  <App />,
   document.getElementById('root')
 ); 
