c1548661a9bb23cc363b32533aaaf4f6b56fd346
Carlos Green <zonxa14@gmail.com>
2018-11-25 13:42:42 -0800

    Update 9.js

--- curriculum/js1/9.js
+++ curriculum/js1/9.js
@@ -4,13 +4,40 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
-    let next;
-    for (let i = a + 1; true; i++) {
-          if (i % 7 === 0) {
-                  next = i; break;
-                }
-        }
-    return next;
-};
+// FOR LOOP
+
+// const solution = (a) => {
+//     for (let i = a + 1; true; i++) {
+//           if (i % 7 === 0) {
+//                   return i;
+//                 }
+//         }
+// };
+
+// WHILE LOOP
+
+// const solution = (a) => {
+//   i = a + 1;
+//   while (true) {
+//     if (i % 7 === 0) {
+//       return i;
+//     }
+//     i++;
+//   }
+// }
+
+// RECURSION
+
+const solution = (a, b = a + 1) => {
+  if (b % 7 === 0) {
+    return b;
+  }
+  b++;
+  return solution(a, b);
+}
+
+
+console.log(solution(1)); // 7
+console.log(solution(14)); // 21
+console.log(solution(21)); // 28
+console.log(solution(28)); // 35
