4fa8c9e278633d09c89cc1588e461d5fe3a3adf7
Carlos Green <zonxa14@gmail.com>
2018-11-25 14:31:53 -0800

    Update 5.js

--- curriculum/js2/5.js
+++ curriculum/js2/5.js
@@ -4,11 +4,51 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
-};
+// FOR LOOP
 
-module.exports = {
-  solution,
-};
+// const solution = (a) => {
+// let high = a[0];
 
+//   for (var i = 0; i < a.length; i++) {
+//     if (a[i] > high) {
+//       high = a[i];
+//     }
+//   }
+//   return high;
+// };
+
+// WHILE LOOP
+
+// const solution = (a) => {
+//   let high = a[0];
+//   i = 0;
+
+//   while (i < a.length) {
+//     if (a[i] > high) {
+//       high = a[i];
+//     }
+//     i++;
+//   }
+//   return high;
+// }
+
+
+// RECURSION
+
+const solution = (a, b = a[0], c = 0) => {
+  if (c === a.length) {
+    return b;
+  }
+  if (a[c] > b) {
+    b = a[c];
+  }
+  c++;
+  return solution(a, b, c);
+}
+
+
+console.log(solution([6, 7])); // 7
+console.log("-----");
+console.log(solution([3, -13])); // 3
+console.log("-----");
+console.log(solution([-3, -8, -5])); // -3
