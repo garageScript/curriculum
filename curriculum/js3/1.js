fea807cceb0a555618ec3a323f2889ec3fe1ad0d
Carlos Green <zonxa14@gmail.com>
2018-11-25 17:04:43 -0800

    Update 1.js

--- curriculum/js3/1.js
+++ curriculum/js3/1.js
@@ -4,11 +4,43 @@
  * @returns {number} max
  */
 
-const solution = (obj)=>{
-  return 0;
+// FOR LOOP
+
+const solution = (obj) => {
+  let count = 0;
+
+  for (i in obj) {
+    if (obj[i] > count) {
+      count = obj[i];
+    }
+  }
+  return count;
 }
 
-module.exports = {
-  solution
-}
+console.log(solution({
+          a: 1,
+          b: 2,
+          c: 3,
+          d: 4,
+          e: 5
+        })); // 5
 
+console.log(solution({
+          a: 10,
+          b: 5,
+          c: 3,
+          d: 6
+        })); // 10
+
+console.log(solution({
+          a: -10,
+          b: -20,
+          c: -30,
+          d: -40
+        })); // -10
+
+console.log(solution({
+          a: 0,
+          b: 0,
+          c: 0
+        })); // 0
