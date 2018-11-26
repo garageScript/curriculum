26dd2d93d2fdefe3099592e2c745b29dddd6b7a6
Carlos Green <zonxa14@gmail.com>
2018-11-25 16:54:36 -0800

    Update 6.js

--- curriculum/js2/6.js
+++ curriculum/js2/6.js
@@ -4,11 +4,56 @@
  * @returns {array}
  */
 
-const solution = (a) => {
-  return [];
-};
+// FOR LOOP
 
-module.exports = {
-  solution,
-};
+// const solution = (a) => {
+//   let arr = [];
 
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] > 5) {
+//       arr.push(a[i]);
+//     }
+//   }
+//   return arr;
+// };  
+
+
+
+// WHILE LOOP
+
+// const solution = (a) => {
+//   let arr = [];
+//   let i = 0;
+
+//   while(i < a.length) {
+//     if (a[i] > 5){
+//       arr.push(a[i]);
+//     }
+//     i++;
+//   }
+//   return arr;
+// }
+
+
+
+
+// RECURSION
+
+
+const solution = (a, b = [], c = 0) => {
+  if (c === a.length) {
+    return b;
+  }
+  if (a[c] > 5) {
+    b.push(a[c]);
+  }
+  c++;
+  return solution(a, b, c);
+}
+
+
+
+console.log(solution([])); // []
+console.log(solution([6, 7])); // [6, 7]
+console.log(solution([3, 4])); // []
+console.log(solution([3, 8, -5])); // [8]
