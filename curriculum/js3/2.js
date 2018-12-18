36aefbb55b131e33375f00bb7c1452c949eb2df6
Carlos Green <zonxa14@gmail.com>
2018-12-17 18:26:46 -0800

    Update 2.js

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -8,20 +8,17 @@
 // FOR LOOP 
 
 const solution = (arr) => {
-  let common = 0;
-  let counter = 0;
-
+  let num = 0;
   for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        counter++;
+      for (var k = 0; k < arr.length; k++) {
+        if (i === k && i === j && k === j) {
+          num = arr[i];
+        }
       }
     }
-    if () {
-      common = arr[i];
-    }
   }
-  return common;
+  return num;
 };
 
 // RECURSION 
@@ -30,8 +27,8 @@ const solution = (arr) => {
 
 
 
-console.log(soltion([1, 2, 2, 3, 4, 4, 4])); // 4
-console.log(soltion([-1, -1, -1, 2, 2, 5, 5])); // -1
-console.log(soltion([0, 0, 1, 2, 4, 0])); // 0
-console.log(soltion([0, 10, 10, 10, 20, 20, 20, 20, 0, 100, 100])); // 20
-console.log(soltion([4, 1, 4, 2, 2, 3, 4])); // 4
+console.log(solution([1, 2, 2, 3, 4, 4, 4])); // 4
+console.log(solution([-1, -1, -1, 2, 2, 5, 5])); // -1
+console.log(solution([0, 0, 1, 2, 4, 0])); // 0
+console.log(solution([0, 10, 10, 10, 20, 20, 20, 20, 0, 100, 100])); // 20
+console.log(solution([4, 1, 4, 2, 2, 3, 4])); // 4
