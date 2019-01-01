6d6cb9b12c6d2b4f78383fff04ef68de52298b74
Carlos Green <Zonxa14@gmail.com>
2019-01-01 15:52:35 -0800

    Update 9.js

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -3,27 +3,87 @@
  * @returns {[]}
  */
 
+var array = [4,5,6,7,8];
+var singleVal = 0;
+var singleVal = array.reduce(function(ac, currentVal) {
+  return ac + currentVal;
+}, 0);
+
+console.log(singleVal);
+
+// console.log([1, 2, 3].reduce((ac, cv) => ac + cv)); // 6
+
+var array = [4,5,6,7,8];
+var singleVal2 = 0;
+for(var i = 0; i < array.length; i++){
+  singleVal2 += array[i];
+}
+
+console.log(singleVal2)
+
 const solution = () => {
-  Array.prototype.gsReduce = function () {
-    // To get the actual array, use this
-    return [];
+  Array.prototype.gsReduce = function(fn, ac = 0, cv, ci = 0, arr) {
+    if (ci === this.length) return ac; // ????????
+    for (var i = 0; i < this.length; i++) {
+      ac += fn(this[ci]);
+      ci += 1;
+    }
+    return this.gsReduce(fn, ac, cv, ci + 1, arr);
   };
 };
 
+// const solution = () => {
+//   Array.prototype.gsReduce = function(ac = 0, cv = 0, ci = 0, arr) {
+//     if (ci === this.length) return ac;
+//       ac += this[ci];
+//     }
+//     return this.gsReduce(ac, cv + 1, ci + 1, arr);
+//   };
 
-// EXAMPLE REDUCE 
+solution();
 
-const sum = [1, 2, 3].reduce(function (result, item){
-  return result + item;
-}, 0);
-
-console.log(sum); // 6
+[1, 2, 3].gsReduce((ac, cv) => ac + cv);
 
 
-console.log(solution([1, 2, 3])); // 6
-console.log(solution([1, 2, 3])); // 4
-console.log(solution(["one", "two", "three"]));
+
+// solution();
+
+// console.log([1, 2, 3].gsReduce((ac, cv) => ac + cv)); // 6
+
+
+// console.log([1, 2, 3].reduce(ac, cv => ac + cv)); // 6
+// console.log([1, 2, 3].reduce(ac, cv => ac + cv + (cv % 2 === 0))); // 5
+
+
+// console.log([1, 2, 3].reduce((ac, cv) => ac + cv)); // 6
+
+
+// the second test for gsReduce is wrong
+
+// it('should reduce [1,2,3] to 5', () => {
+//   const cb = (ac, cv, ci) => {
+//     return ac + cv + (cv % 2 === 0);
+//   };
+//   expect([1,2,3].gsReduce(cb, -3)).to.equal(4);
+// });
+
+// shouldn't it be?
+
+// it('should reduce [1,2,3] to 5', () => {
+//   const cb = (ac, cv, ci) => {
+//     return ac + ci + (cv % 2 === 0);
+//   };
+//   expect([1,2,3].gsReduce(cb, -3)).to.equal(5);
+// });
+
+// console.log([1, 2, 3].reduce((ac , cv, ci) => ac + ci + (cv % 2 === 0))); // 5
+
+
+
+// console.log(solution([1, 2, 3])); // 6
+// console.log(solution([1, 2, 3])); // 4
+// console.log(solution(["one", "two", "three"]));
 //  one: "1 of 3", two: "2 of 2", three: "3 of 3" }'
 
-console.log(solution(["bob", "obo", "boo"])); 
+// console.log(solution(["bob", "obo", "boo"])); 
 // { bob: true, obo: false, boo: true }
