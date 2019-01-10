b5d7d8579ff0be7fc8e852d7ea5d23e358767ca1
Carlos Green <zonxa14@gmail.com>
2019-01-10 04:43:38 -0800

    updates

--- curriculum/js2/12.js
+++ curriculum/js2/12.js
@@ -11,6 +11,8 @@ console.log("​[1, 20, 300].filter(x => x > 6);", [1, 20, 300].filter(x => x > 
 const solution = () => {
     Array.prototype.gsFilter = function(fn, item, filtered = [], index = 0, remainingItems = this) {
 
+        // if (this.length === index) return filtered;
+        if (remainingItems.length === 0) return filtered;
         console.log("remainingItems = ", remainingItems)
         item = remainingItems.shift();
         console.log("item = ", item);
@@ -20,7 +22,6 @@ const solution = () => {
         }
         console.log("filtered = ", filtered);
 
-        if (remainingItems.length === 0) return filtered;
         console.log("index = ", index);
 
     return this.gsFilter(fn, item, filtered, index + 1, remainingItems);
@@ -32,14 +33,14 @@ solution();
 console.log(solution([1, 20, 300].gsFilter(x => x > 6))); // [20, 30]
 console.log("----");
 console.log(solution([0, 1, 2, 3, 4, 5].gsFilter(x => x > 2))); // [3, 4, 5]
-// console.log(solution(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'].gsFilter(x => x.length > 6)));
-// console.log(solution(['hello', 'world', '!'].gsFilter(x => x.length > 2)));
+console.log("----");
+console.log(solution(['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'].gsFilter(x => x.length > 6)));
+console.log("----");
+console.log(solution(['hello', 'world', '!'].gsFilter(x => x.length > 2)));
 
 // console.log(([10, 20, 300].filter(x => x > 11))); // [20, 30]
 // console.log(([1, 2, 3, 4, 5].filter(x => x > 2))); // [3, 4, 5]
-
 // console.log((['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'].filter(x => x.length > 6)));
 // ["exuberant", "destruction", "present"]
-
 // console.log((['hello', 'world', '!'].filter(x => x.length > 2)));
 // ["hello", "world"]
