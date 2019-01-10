9a795b355cc5df46bc44200e4050c76a11d74440
Carlos Green <zonxa14@gmail.com>
2019-01-09 17:48:17 -0800

    Update 10.js

--- curriculum/js2/10.js
+++ curriculum/js2/10.js
@@ -6,9 +6,6 @@
  *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
  */
 
-
-
-
 // TODO: MASTER CHEN'S SOLUTION
 
 // function gsforeach(cb) {
@@ -19,7 +16,6 @@
 // console.log(gsforeach([1, 2, 3, 4, 5]));
 // console.log(gsforeach([6, 7, 8, 9, 10]));
 
-
  /*
 const gsforeach = ((cb,i=0) => {
   if (i === this.length-1) return;
@@ -28,40 +24,43 @@ const gsforeach = ((cb,i=0) => {
 })
  */
 
-// Array.prototype.gsForEach = gsforeach;
+const items = [1, 2, 3];
+const copy = [];
 
+//  FOR LOOP
 
-// const cb = ((el, idx) => console.log(`a[${idx}] = ${el}`));
+// for (let i = 0; i < items.length; i++) {
+  // console.log(items[i], i);
+// }
 
-
+// RECURSION 
 
 const solution = () => {
-  Array.prototype.gsForEach = function (fn, element, index) {
+  Array.prototype.gsForEach = function (fn, element = this[0], index = 0) {
     // To get the actual array, use this
-    if (index === this.length) return total;
+    // set default element to this[0]
+    // set index to 0
+    if (index === this.length) return element;
+    // if index equals this.length reutrn element
 
-    total = fn(element, index);
+    element = fn(this[index], index);
+    // element equals the function wrapped around this[index], index
 
-    return this.gsForEach(fn, element, index);
+    return this.gsForEach(fn, element, index + 1);
+    // recursive call indexing by 1
   };
 };
 
+solution();
 
 // EXAMPLE FOR EACH
 
-[1, 2, 3].forEach(function (item, index){
-  console.log(item, index);
-}); 
+// [1, 2, 3].forEach(function (item, index){
+//   console.log(item, index);
+// }); 
 // 0 1
 // 2 1 
 // 3 2
-
-
-// tests
-
-console.log(solution([1, 2, 3]));
-// 1: 3
-// 2: 4
-// 3: 5
-console.log(solution([1, 2, 3]));
-// 3
+[1, 2, 3].gsForEach(function (element, index) {
+  console.log(element, index);
+});
