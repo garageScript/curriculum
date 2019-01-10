e9340ca697d3336723378b3e8d6b8c56cc2bc086
Carlos Green <zonxa14@gmail.com>
2019-01-09 17:52:14 -0800

    Update 12.js

--- curriculum/js2/12.js
+++ curriculum/js2/12.js
@@ -11,14 +11,19 @@ console.log("​[1, 20, 300].filter(x => x > 6);", [1, 20, 300].filter(x => x > 
 // 300
 
 const solution = () => {
-    Array.prototype.gsFilter = function() {
+    Array.prototype.gsFilter = function(fn, item = this[0], index = 0) {
 
+        if (index === this.length) return item;
+
+        item = fn(this[index], index);
+
+    return this.gsFilter(fn, item, index + 1);
     }
 }
 
 
 
-// solution();
+solution();
 
 console.log(solution([].gsFilter(x => x > 6))); // [20, 30]
 console.log(solution([].gsFilter(x => x > 2))); // 
