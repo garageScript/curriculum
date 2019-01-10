540a1b9ebbb92a7f60b6fdcfb025e1f007542f51
Carlos Green <zonxa14@gmail.com>
2019-01-09 17:09:17 -0800

    Update 9.js

--- curriculum/js2/9.js
+++ curriculum/js2/9.js
@@ -2,40 +2,34 @@
  * Replicate Array.prototype.reduce and call it gsReduce
  * @returns {[]}
  */
-
 const solution = () => {
   // fn = (accumulator, item, index) => accumulator
-  Array.prototype.gsReduce = function (fn, accumulator, item, index = 0, remainingItems = this) {
-    item = remainingItems.shift();
-    
-    accumulator = fn(accumulator, item)
-    
-    if (remainingItems.length === 0)  {
+  Array.prototype.gsReduce = function (fn, accumulator = 0, item, index = 0, remainingItems = this) {
+    // set the default accumulator to 0
+    // deafult index to 0
+    // default remaining items to this
+  item = remainingItems.shift();
+  // pop off the first element in the array
+  accumulator = fn(accumulator, item, index)
+  // pass fn or function onto the accumulator, item and index
+
+  if (remainingItems.length === 0)  {
+    // if the remaining items array doesn't have any elements in it
       return accumulator;
+      // return accumulator
     }
 
     return this.gsReduce(fn, accumulator, item, index + 1, remainingItems);
+    // pass the recursive call while indexing index
   }
 };
 
-
 solution();
 
-// console.log(" ");
-// console.log("---reduce----");
 console.log([10, 20, 30, 40, 50].reduce((ac, item) => ac += item));
-// console.log(" ");
-
-// console.log("---GS REDUCE---");
-console.log(" ");
-console.log([10, 20, 30, 40, 50].gsReduce((accumulator, item) => accumulator += item));
 console.log(" ");
 
-// let arr2 = ["cat", "dog", "mouse"];
 
-// for (let i = 0; i < arr2.length; i++) {
-  // console.log(arr2[i])
-// }
-
-[1, 2, 3].map(x => x + 1);
-
+console.log(" ");
+console.log([10, 20, 30, 40, 50].gsReduce((ac, item) => ac += item));
+console.log(" ");
