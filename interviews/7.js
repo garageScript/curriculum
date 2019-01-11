a6042b8936da3bc51e7580f82cc056c389dabdd8
jds1337 <jds1337@garagescript.org>
2019-01-11 11:26:51 -0800

    refactored rotated sorted array solution and test

--- interviews/7.js
+++ interviews/7.js
@@ -1,13 +1,15 @@
 /** Write a function that takes in a rotated sorted array and return the smallest number
- * @param {array} 
+ * @param {array}
  * @returns {number}
  */
 
 const solution = (a, s = 0, e = a.length - 1, m = Math.floor((s + e) / 2)) => {
-  if (a[m] < a[s] && a[m] < a[e]) return a[m]
-  if (s === m && a[e] < a[s]) return a[e]
-  if (a[m] > a[s] && a[m] > a[s]) {
-    return solution(a, s = m, e, m = Math.floor((s + e) / 2))
+  if (a[s] < a[e] && s === m) return a[s]
+  if (a[e] < a[s] && s === m) return a[e]
+  if (a[s] > a[e]) {
+    s = m
+    m = Math.floor((s + e) / 2)
+    return solution(a, s, e, m)
   }
 }
 
--- interviews/__tests__/7.js
+++ interviews/__tests__/7.js
@@ -8,7 +8,7 @@ describe('find minimum value in rotated sorted array', () => {
     expect(typeof result).to.equal('number')
   })
 
-  it('should handle positive numbers in array', () => {
+  it('positive numbers in array', () => {
     const result = solution([5, 6, 7, 8, 9, 2, 3])
     expect(result).to.equal(2)
   })
@@ -18,18 +18,33 @@ describe('find minimum value in rotated sorted array', () => {
     expect(result).to.equal(1)
   })
 
-  it('should handle [2, 4, 6, 1]', () => {
+  it('[3, 4, 5, 1, 2] should return 1', () => {
+    const result = solution([3, 4, 5, 1, 2])
+    expect(result).to.equal(1)
+  })
+
+  it('[2, 4, 6, 1] should return 1', () => {
     const result = solution([2, 4, 6, 1])
     expect(result).to.equal(1)
   })
 
-  it('should handle [2, 3, 4, 6, 1]', () => {
+  it('[2, 3, 4, 6, 1] should return 1', () => {
     const result = solution([2, 3, 4, 6, 1])
     expect(result).to.equal(1)
   })
 
-  it('should handle [3, 4, 5, 6, 8, 9, 1, 2]', () => {
+  it('[3, 4, 5, 6, 8, 9, 1, 2] should return 1', () => {
     const result = solution([3, 4, 5, 6, 8, 9, 1, 2])
     expect(result).to.equal(1)
+  })
+
+  it('[5, 4, 3, 1, 2] should return 1', () => {
+    const result = solution([5, 4, 3, 1, 2])
+    expect(result).to.equal(1)
+  })
+
+  it('[9, 6, 5, 4, 2, 3] should return 2', () => {
+    const result = solution([9, 6, 5, 4, 2, 3])
+    expect(result).to.equal(2)
   })
 })
