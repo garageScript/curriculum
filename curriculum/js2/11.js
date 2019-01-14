0b3df07f9543a402a303c636a1b8cda975501424
Carlos Green <zonxa14@gmail.com>
2019-01-14 09:00:53 -0800

    Update 11.js

--- curriculum/js2/11.js
+++ curriculum/js2/11.js
@@ -4,22 +4,9 @@
 
 // Examples:
 
-const solution = (array, index = array.length - 1, accumulator = []) => {
+const solution = (array, accumulator = 0, index = array.length - 1) => {
 
-    let sum = array.reduce((ac, item) => ac += item)
-
-    if ((array.length - 1) === accumulator.length) {
-        accumulator.unshift(sum - (accumulator.length)) // then unshift sum
-        return accumulator; // return 
-    }
-
-    accumulator.unshift(1);
-    // first you distribute 1 in elements at index 1 to arr.length
-    // decrement a
-    // put the remainder at index 0
-
-    return solution(array, index - 1, accumulator);
-};
+}
 
 
 console.log(solution([0, 0, 3])); // returns [1,1,1]
@@ -28,3 +15,20 @@ console.log(solution([0, 1, 3, 5])); // returns [6,1,1,1]
 console.log(solution([1, 5])); // returns [5,1]
 console.log(solution([5, 2])); // returns [6,1]
 console.log(solution([5, 1])); // returns [5,1]
+
+// const solution = (array, index = array.length - 1, accumulator = []) => {
+
+//     let sum = array.reduce((ac, item) => ac += item)
+
+//     if ((array.length - 1) === accumulator.length) {
+//         accumulator.unshift(sum - (accumulator.length)) // then unshift sum
+//         return accumulator; // return 
+//     }
+
+//     accumulator.unshift(1);
+//     first you distribute 1 in elements at index 1 to arr.length
+//     decrement a
+//     put the remainder at index 0
+
+//     return solution(array, index - 1, accumulator);
+// };
