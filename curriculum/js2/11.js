d7c960232c365af6285877d286b1218432286083
Carlos Green <zonxa14@gmail.com>
2019-01-14 10:54:54 -0800

    Update 11.js

--- curriculum/js2/11.js
+++ curriculum/js2/11.js
@@ -1,13 +1,22 @@
-
 // Given an array, redistribute values greater than 1 to 
 // previous elements that are 0 and change it to 1. All remainders will accumulate at index 0.
 
 // Examples:
 
 const solution = (array, accumulator = 0, index = array.length - 1) => {
+    if (index === 0) {
+        array[0] += accumulator;
+        return array;
+    }
 
+    if (array[index] != 1) {
+        accumulator += array[index];
+        array[index] = 1;
+        accumulator -= 1
+    }
+
+    return solution(array, accumulator, index - 1);
 }
-
 
 console.log(solution([0, 0, 3])); // returns [1,1,1]
 console.log(solution([0, 1, 3])); // returns [2,1,1]
@@ -15,20 +24,3 @@ console.log(solution([0, 1, 3, 5])); // returns [6,1,1,1]
 console.log(solution([1, 5])); // returns [5,1]
 console.log(solution([5, 2])); // returns [6,1]
 console.log(solution([5, 1])); // returns [5,1]
-
-// const solution = (array, index = array.length - 1, accumulator = []) => {
-
-//     let sum = array.reduce((ac, item) => ac += item)
-
-//     if ((array.length - 1) === accumulator.length) {
-//         accumulator.unshift(sum - (accumulator.length)) // then unshift sum
-//         return accumulator; // return 
-//     }
-
-//     accumulator.unshift(1);
-//     first you distribute 1 in elements at index 1 to arr.length
-//     decrement a
-//     put the remainder at index 0
-
-//     return solution(array, index - 1, accumulator);
-// };
