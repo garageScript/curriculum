7a188f8bd498c45b4a98f88499b76e1418c5758b
Carlos Green <zonxa14@gmail.com>
2019-01-14 17:12:38 -0800

    Update 4.js

--- curriculum/js3/4.js
+++ curriculum/js3/4.js
@@ -6,13 +6,36 @@
  * @returns {boolean}
  */
 
-// RECURISON
+// FOR LOOP
 
 // const solution = (a, b) => {
+// if (a.length < 1) return false;
 
+//     for (var i = 0; i < a.length; i++) {
+//         for (var j = i + 1; j < a.length; j++) {
+//             if ((a[i] + a[j]) === b) {
+//                 console.log(a[i], a[j]);
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
 // }
 
-// console.log(solution([], 0)) // false
-// console.log(solution([1, 4, 3, 2], 6)) // true
-// console.log(solution([-1,-3,-4], -2)); // false
-// console.log(solution([0,0,0], 0)); // true
+// RECURSION
+
+const solution = (a, b, i = 0, j = i + 1) => {
+if (i === a.length || a.length < 1) return false;
+    
+    if ((a[i] + a[j]) === b)  return true; 
+
+    return solution(a, b, i + 1, j)
+}
+
+console.log(solution([], 0)) // false
+console.log("----");
+console.log(solution([1, 4, 3, 2], 6)) // true
+console.log("----");
+console.log(solution([-1,-3,-4], -2)); // false
+console.log("----");
+console.log(solution([0,0,0], 0)); // true
