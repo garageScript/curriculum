320be7095d2bd67834f3d0b51b81076301dd3c6b
Carlos Green <zonxa14@gmail.com>
2019-01-19 20:23:37 -0800

    Updates

---
+++ js0/1.py
@@ -0,0 +1,8 @@
+"""
+ * Takes in 2 numbers, returns their sum
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+"""
+
+print("Hello world")
---
+++ js0/1.rb
@@ -0,0 +1,10 @@
+#  * Takes in 2 numbers, returns their sum
+#  * @param {number} a
+#  * @param {number} b
+#  * @returns {number}
+
+def solution 
+
+end
+
+p "Hello World"
