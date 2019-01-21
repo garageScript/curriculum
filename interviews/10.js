84a27a2f8f37b4aa6ba8b2cce0884953c89491b3
jds1337 <song@zheng.club>
2019-01-21 13:21:54 -0800

    fibonacci solution and test

--- interviews/10.js
+++ interviews/10.js
@@ -3,6 +3,8 @@ const solution = (n) => {
   let a = 1
   let next
   const res = [0, 1]
+  if (n === 2) return res
+  if (n === 1) return [b]
   while (n !== res.length) {
     next = a
     a = a + b
--- interviews/__tests__/10.js
+++ interviews/__tests__/10.js
@@ -2,7 +2,7 @@
 const expect = require('chai').expect
 const solution = require('../10').solution
 
-describe('Return an array of Fibonacci numbers, given starting elements [0, 1]', () => {
+describe('Return an array of Fibonacci numbers, given starting sequence [0, 1]', () => {
   it('Given an integer n, return an array of the first n numbers of fibonacci sequence', () => {
     const result = solution(4)
     expect(result).to.deep.equal([0, 1, 1, 2])
@@ -16,5 +16,15 @@ describe('Return an array of Fibonacci numbers, given starting elements [0, 1]',
   it('Given 8, should return an array of the first 8 numbers of fibonacci sequence', () => {
     const result = solution(8)
     expect(result).to.deep.equal([0, 1, 1, 2, 3, 5, 8, 13])
+  })
+
+  it('Given 2, should return an array of the first 2 numbers of fibonacci sequence', () => {
+    const result = solution(2)
+    expect(result).to.deep.equal([0, 1])
+  })
+
+  it('Given 1, should return an array of the first 1 number of fibonacci sequence', () => {
+    const result = solution(1)
+    expect(result).to.deep.equal([0])
   })
 })
