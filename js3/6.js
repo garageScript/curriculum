3f1ccc4e146567c0e861cfbc128de1e2571f05c1
Carlos Green <zonxa14@gmail.com>
2019-01-23 13:17:24 -0800

    Update 6.js

--- js3/6.js
+++ js3/6.js
@@ -2,7 +2,7 @@
  * filter - takes in an object and a function. Return a new object
  * with key/value if the function returns true.
  * Example:
- * input: {5:'blah blah',name:'ho', zolo:'4thech'},(k,v)=>{return v.length > 3}
+ * input: {5:'blah blah', name:'ho', zolo:'4thech'},(k,v)=>{return v.length > 3}
  * output: {5:'blah blah', zolo:'4thech'}
  * @param {object} a
  * @param {function} b
@@ -11,24 +11,10 @@
 
 
 const solution = (a, fun, obj = {}, i = 0) => {
-  console.log('a = ', a);
-
-  if (i === 0) {
-  a = Object.entries(a);
-  }
-  console.log('i =', i);
-  console.log('a = ', a);
-  console.log("obj =", obj)
-
-  if (i === a.length) return obj;
-
-  obj.k = a[i][0];
-  console.log("obj.k = ", obj.k);
-  obj.v = a[i][1];
-  console.log("obj.v =", obj.v);
-  console.log("obj =", obj);
-  // fun(obj.k, obj.v);
-  console.log('-----');
+  if (i === Object.entries(a).length) return obj;
+  const k = Object.keys(a)[i];
+  const v = Object.values(a)[i];
+  if (fun(k, v))obj[k] = v;
   return solution(a, fun, obj, i + 1);
 };
 
