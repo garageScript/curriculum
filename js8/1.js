d1aaf90151ff07907d23030a1787b865464a2c44
Carlos Green <zonxa14@gmail.com>
2019-01-26 17:39:07 -0800

    Update 1.js

--- js8/1.js
+++ js8/1.js
@@ -1,9 +1,11 @@
 /* Given a binary tree, find the largest value
-Example:
+* @param {object} a
+* @returns {number}
+
+Example Tree:
         (30)
         / \
     (50)   (40)
-    tree
 
 const a = {
     val: 30,
@@ -14,6 +16,22 @@ const a = {
         val: 50
     }
 };
-solution(a); 
+solution(a)
 should return 50 */
 
+const solution = (node, largest = 0) => {
+    console.log("node =", node);
+    console.log("largest =", largest);
+    console.log("node.val =", node.val);
+    console.log("node.left =", node.left);
+    console.log("node.right =", node.right);
+
+    if (node.val > largest) largest = node.val;
+    if (!node.left || !node.right) return largest;
+    node = (node.left || node.right);
+    return solution(node, largest);
+};
+
+module.exports = {
+    solution,
+};
