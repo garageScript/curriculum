ac040b3feeaae67c629f8fffb949cb28d04d29f6
jds1337 <jds1337@garagescript.org>
2019-01-29 11:40:42 -0800

    return an array of n fibonacci num in array boiler-plate

--- interviews/10.js
+++ interviews/10.js
@@ -1,17 +1,5 @@
 const solution = (n) => {
-  let b = 0
-  let a = 1
-  let next
-  const res = [0, 1]
-  if (n === 2) return res
-  if (n === 1) return [b]
-  while (n !== res.length) {
-    next = a
-    a = a + b
-    res.push(a)
-    b = next
-  }
-  return res
+  return 0
 }
 
 module.exports = {
