016c8f60196abe6090c411626e850fc68756b92a
jds1337 <jds1337@garagescript.org>
2019-01-31 09:55:29 -0800

    added problem description

--- interviews/13.js
+++ interviews/13.js
@@ -1,3 +1,16 @@
+/*
+ * In computer science, a cookie refers to some data that is sent from a website and is stored on a client's computer while they are browsing.
+ * It is transmitted from the website as a string that is prased by the user's browser into perspective key-value pairs.
+ *
+ * A cookie string can only be parsed if it is "valid".
+ * In a "valid" cookie string, each key and value are separated by an equals sign (=) and each pair but the last is terminated by a semi-colon(;).
+ *
+ * Write a function and use regular expression to find whether the cookie string is valid. Return true if cookie string is valid, false if cookie string is not valid.
+ *
+ * @params{String}
+ * @returns{Boolean}
+*/
+
 const solution = (str) => {
   const reWithSemi = /\w+=\w+[;]$/
   const reWithoutSemi = /\w+=\w+[^;]$/
