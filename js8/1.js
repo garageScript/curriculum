842b6728f0efc6a29f770225f07997ee7ea6874f
Carlos Green <zonxa14@gmail.com>
2019-02-03 09:43:29 -0800

    Update 1.js

--- js8/1.js
+++ js8/1.js
@@ -7,74 +7,35 @@ Example Tree:
         / \
     (50)   (40)
 */
-// const a = {
-//     val: 30,
-//     right: {
-//         val: 40,
-//     },
-//     left: {
-//         val: 50,
-//     },
-// };
-
-// const solution = (node, largest = -1000) => {
-//     if (!node) return largest;
-//     console.log("node =", node);
-
-//     console.log("largest =", largest);
-//     solution(node.left);
-    
-//     console.log("node.val =", node.val);
-//     if (largest < node.val) largest = node.val;
-//     solution(node.right);
-// };
-
-// console.log(solution(a));
-
-// module.exports = {
-//     solution,
-// };
-
-
-// A = {
-//     value: 15,
-//     children: [b, c, d],
-// };
-
-// B = {
-//     value: 12,
-//     children: [e],
-// };
-
-const b = {
-    val: 40,
-};
-
-const c = {
-    val: 50,
-};
-
 const a = {
     val: 30,
-    children: b, c,
-};
-console.log(a);
-
-const bft = (n, largest = -1000) => {
-    if (!n) return;
-    console.log('Breadth First Traversal = ', n.val);
-    if (largest < n.val) largest = n.val;
-    c = c.concat(n.children || []);
-    return bft(c.shift(), c);
+    right: {
+        val: 40,
+    },
+    left: {
+        val: 50,
+    },
 };
 
-const dfs = (n, largest = -1000) => {
-    if (!n) return;
-    console.log('Depth First Search = ', n.v);
-    c = c.concat(n.children || []);
-    return dfs(c.pop(), c);
+const solution = (node, largest = -1000) => {
+    if (!node) return largest;
+    // node = node.right;
+    // solution(node.right);
+    solution(node.right, largest);
+    if (largest < node.val)  {
+        largest = node.val; 
+    }
+    solution(node.left, largest);
+
+    console.log("node = ", node);
+    console.log("node.val =", node.val);
+    console.log("largest =", largest);
+    console.log("----");
 };
 
-console.log(bft(a));
-console.log('-----');
-console.log(dfs(a));
+console.log(solution(a));
+
+module.exports = {
+    solution,
+};
+
