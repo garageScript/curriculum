f68546b1df4974f3e5241065f8e7cce367c2f576
Carlos Green <zonxa14@gmail.com>
2019-03-20 00:21:51 -0700

    js4

--- js4/1.js
+++ js4/1.js
@@ -2,14 +2,14 @@
 // Write a script that console.logs all the users inside the
 // /home folder that is not you. Use fs.readdir and read /home directory.
 
-const express = require('express')
-const app = express()
 const fs = require('fs')
-const printNames = (err, data) => {
-// my username === 'greenc'
+
+fs.readdir('/home', (err, data) => {
+  if (err) throw err
+  data.forEach((x) => {
+  // my username === 'greenc'
   if (x !== 'greenc') {
     console.log(x)
     }
   })
-}
-fs.readdir('/home', printNames)
+})
--- js4/2.js
+++ js4/2.js
@@ -1,8 +1,5 @@
 // Getting To Know Your Computer, pt. 2 - Write All Except You
 // Write a script that creates a file ~/users.txt that contains all the users inside the /home folder that is not you.
-
-// fs.writeFile('fileName', 'fileData', () => {});
-
 const fs = require('fs')
 
 fs.readdir('/home', (err, data) => {
