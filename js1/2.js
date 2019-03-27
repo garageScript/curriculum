de633a8443defda4c82da4ddb149334184df249c
Carlos Green <zonxa14@gmail.com>
2019-03-27 13:30:31 -0700

    Update 2.js

--- js1/2.js
+++ js1/2.js
@@ -1,19 +1,22 @@
  /**  
- * Write a function that takes in a number (X) and a function (A),
-and returns a function (B). Whenever the function (B) is called (up to X times), 
+ * Write a function that takes in a number (X)
+and a function (A),
+and returns a function (B). Whenever the function
+(B) is called (up to X times), 
 the result of function (A) is returned .
-After that, null will be returned whenever the function (B) is called.               
- * @param {number} a                                                                                                                     
- * @param {function} b                                                                                                                   
- * @returns {function}                                                                                                                   
- */
+After that, null will be returned whenever the
+function (B) is called.               
+ * @param {number} a
+ * @param {function} b
+ * @returns {function}
+**/
 
 const solution = (num, func) => {
   if (num === 0) return null
     return () => {
     func()
+    return solution(num - 1, func)
   }
-  return solution(num - 1, func)
 }
 
  module.exports = {
