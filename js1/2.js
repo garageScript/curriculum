15b99c3cd30886ebf1560c47770da85cea347b81
Carlos Green <zonxa14@gmail.com>
2019-03-28 10:31:20 -0700

    Update 2.js

--- js1/2.js
+++ js1/2.js
@@ -11,13 +11,42 @@ function (B) is called.
  * @returns {function}
 **/
 
-const solution = (num, func) => {
+const solution = (num, func, i = 0) => {
+  if (num < 1) return null
   if (num === 0) return null
     return () => {
-    func()
-    return solution(num - 1, func)
+   func()
   }
+  return solution(num, func, i + 1)
 }
+
+/*
+
+
+
+const sayHelloTimes = (name, n, i = 0) => {
+   if (i === n) return
+  const greet = () => {
+   console.log(`Hi, ${name}`)
+  }
+   if (i < n) greet()
+  return sayHelloTimes(name, n, i + 1)
+}
+
+console.log(sayHelloTimes('Chen', 5))
+
+ const solution = (arr, sum = 1) => {
+   const summer = (i = 0) => {
+     if (i === arr.length) return sum
+     if (i < arr.length) { sum *= arr[i] }
+    return summer(i + 1)
+   } 
+  summer()
+  return sum
+} 
+const array = [1, 2, 3, 4] 
+console.log(solution(array))
+*/
 
  module.exports = {
    solution
