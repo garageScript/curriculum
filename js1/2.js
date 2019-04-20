9546d94d7658f2de0f8bb1feb5fdc929cd48fb45
Theodore <ted-zen@garagescript.org>
2019-04-20 16:13:53 -0700

    2 solved

--- js1/2.js
+++ js1/2.js
@@ -1,4 +1,4 @@
-/**
+/* jjjjjjjjjkjkk*
  * Write a function that takes in a number (X) and a function (A), and returns a function (B). Whenever the function (B) is called (up to X times), the result of function (A) is returned . After that, null will be returned whenever the function (B) is called.
  * @param {number} a
  * @param {function} b
@@ -6,7 +6,11 @@
  */
 
 const solution = (a, b) => {
+  
   return () => {
+    if (a === 0) return null
+    a --
+    return b()
   }
 }
 
