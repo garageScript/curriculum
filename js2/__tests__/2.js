6543ae871c7d4f771548bdecea81442af801dfa4
Song Zheng <sozheng@paypal.com>
2019-04-21 22:29:08 +0800

    fix tests

--- js2/__tests__/2.js
+++ js2/__tests__/2.js
@@ -6,25 +6,25 @@ let solution = require('../2').solution
 describe('Generate array', () => {
   it('should generate 1d array with 0 values', () => {
     const result = solution((e) => {
-      return false
+      return true
     })
     expect(result).to.deep.equal([])
   })
   it('should generate 1d array with 1 values', () => {
     const result = solution((e) => {
-      return e < 1
+      return e > 0
     })
     expect(result).to.deep.equal([0])
   })
   it('should generate 1d array with 5 values', () => {
     const result = solution((e) => {
-      return e < 5
+      return (e % 7 === 0) && e !== 0
     })
-    expect(result).to.deep.equal([0, 1, 2, 3, 4])
+    expect(result).to.deep.equal([0, 1, 2, 3, 4, 5, 6])
   })
   it('should generate 1d array with 10 values', () => {
     const result = solution((e) => {
-      return e < 10
+      return e > 9
     })
     expect(result).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
   })
