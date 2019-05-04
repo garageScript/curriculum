84426b320cceb561f54993c3adf9cbab309c13d7
Theodore <ted-zen@garagescript.org>
2019-05-04 15:27:21 -0700

    3 tail recursion

--- js1/3.js
+++ js1/3.js
@@ -1,14 +1,19 @@
-/**
+/*i*
  * Say Hello - takes in a number x
  *     return string with “hello” repeated x times.
  * @param {number} a
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
-};
+const solution = (a, c = '') => {
+   
+   if(a == 0) {
+    return c
+   }
+   console.log(c)
+   return solution(a-1,c + 'hello');
+  };
 
 module.exports = {
-  solution,
-};
+  solution
+}
