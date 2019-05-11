10843b6dc82a4250afe4f940646041742f2dd678
Theodore <ted-zen@garagescript.org>
2019-05-11 14:46:40 -0700

    clean branch 7

--- js1/7.js
+++ js1/7.js
@@ -11,8 +11,8 @@
 const solution = (inp, letter, i = 0) => { // solution('scp','p')
   if (!inp) return false  // empty string input
   if (inp[i] === letter) return true /* stack lv1: 's' !== 'p' i = 0 inp.length = 3
-                                              lv2: 'c' !== 'p' i = 1 inp.length = 3
-                                              lv3: 'p' === 'p' return true stop execute following line i = 2 inp.length = 3
+                                              lv2: 'c' !== 'p' i = 1     length = 3
+                                             lv3: 'p' === 'p' return true stop execute following line i = 2 inp.length = 3
                                               */
     */
   if (i === inp.length -1) return false
