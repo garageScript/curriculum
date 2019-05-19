50f922b7ebebdf0d80ba4358200a15f672925b92
Song Zheng <song@zheng.club>
2019-05-19 13:04:46 -0700

    add new test for js2 problem 4 so that tests does not fail

--- js2/__tests__/4.js
+++ js2/__tests__/4.js
@@ -5,18 +5,23 @@ const solution = require('../4').solution
 
 describe('Closure forEach', () => {
   it('should always call cb when array length is 1', () => {
+    let calls = 0
     const res = solution([9], (e) => {
       expect(e).to.equal(9)
+      calls += 1
     })
     res()
     res()
     res()
+    expect(calls).to.equal(3)
   })
   it('should call cb with elements in array with size 5', () => {
     let i = 0
     let arr = [8, 9, 4, 6, 7]
+    let calls = 0
     const res = solution(arr, (e) => {
       expect(e).to.equal(arr[i])
+      calls += 1
       i = (i + 1) % arr.length
     })
     res()
@@ -30,5 +35,6 @@ describe('Closure forEach', () => {
     res()
     res()
     res()
+    expect(calls).to.equal(11)
   })
 })
