63c198071a86aa5ba095a735f472f0483ea9613f
Carlos Green <zonxa14@gmail.com>
2019-05-23 13:37:09 -0700

    Update 4.js

--- js2/test/4.js
+++ js2/test/4.js
@@ -3,20 +3,25 @@
 const expect = require('chai').expect
 const solution = require('../4').solution
 
-describe('js2/4.js Closure forEach', () => {
+describe('Closure forEach', () => {
   it('should always call cb when array length is 1', () => {
+    let calls = 0
     const res = solution([9], (e) => {
       expect(e).to.equal(9)
+      calls += 1
     })
     res()
     res()
     res()
+    expect(calls).to.equal(3)
   })
   it('should call cb with elements in array with size 5', () => {
     let i = 0
     let arr = [8, 9, 4, 6, 7]
+    let calls = 0
     const res = solution(arr, (e) => {
       expect(e).to.equal(arr[i])
+      calls += 1
       i = (i + 1) % arr.length
     })
     res()
@@ -30,5 +35,6 @@ describe('js2/4.js Closure forEach', () => {
     res()
     res()
     res()
+    expect(calls).to.equal(11)
   })
 })
