2676704cdbdc9112d0e14f854d2267618bf8c0d3
Carlos Green <zonxa14@gmail.com>
2019-05-24 09:32:08 -0700

    Update 3.js

--- js1/3.js
+++ js1/3.js
@@ -2,26 +2,22 @@
 Write a function called solution that
 takes in 2 parameters, a number n and string,
 and returns the string repeated n number of times
-
  * @param {number} a
  * @param {string} b
  * @returns {string}
  */
 
-
-// FOR LOOP
 /*
+// FOR LOOP
 const solution = (a, b, result = '') => {
    if (a < 1) { return ''; }
    for (var i = 0; i < a; i++) {
      result += b;
    }
    return result;
-};*/
-
+};
 
 // WHILE LOOP
-
 const solution = (a, b, result = '') => {
    i = 0;
      while (i < a) {
@@ -31,15 +27,14 @@ const solution = (a, b, result = '') => {
    return result;
 } 
 
-
 // RECURSION 
-/*
 const solution = (a, b, result = '') => {
   if (a === 0) return result;
 
   return solution(a - 1, b, result += b)
-}*/
+} */
 
+// Built in JS function
 console.log("hello".repeat(6));
 
 console.log(solution(6));
