6b7c716d1a427a5209318033a4038c806b02634d
Carlos Green <zonxa14@gmail.com>
2019-05-30 11:58:58 -0700

    Ms Fat booty

--- js1/10.js
+++ js1/10.js
@@ -1,29 +1,36 @@
 /**
- * Write a function called solution that
- *   takes in 2 parameters, a string and a function,
- *   and returns a function.
- *  When the returned function is called for the first time,
- *    the input function will be called with the first letter of the string.
- *  When the returned function is called for the second time,
- *    the input function will be called with the second letter of the string.
- *  When the returned function is called for the third time,
- *    the input function will be called with the third letter of the string.
- *  ...
- *  When the returned function is called with the last letter of the string,
- *    calling it again will call the input function with the first letter
- *    of the string and start over.
- * @param {string} str
- * @param {function} fun
- * @returns {function}
+Write a function called solution that
+takes in 2 parameters, a string and a function,
+and returns a function.
+
+When the returned function is called for the first time,
+the input function will be called with the first letter
+of the string.
+
+When the returned function is called for the second time,
+the input function will be called with the
+second letter of the string.
+
+When the returned function is called for the third time,
+the input function will be called with the third
+letter of the string.
+*  ...
+When the returned function is called with the last 
+letter of the string,
+calling it again will call the input function
+with the first letter
+the string and start over.
+
+*@param {string} str
+*@param {function} fun
+*@returns {function}
 */
 
-const solution = (string, callBack) => {
-  let i = 0;
-
+const solution = (string, callBack, i = 0) => {
   return () => { 
       if (i === string.length) i = 0;
-      callBack(string[i])
-      i = i + 1	    
+      if (i < string.length) { callBack(string[i]) }
+      i = i + 1
   }
 }	
 
