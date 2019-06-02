28ada6fe3f0e16e4906401d4ff3c13c2f54c6839
Theodore <ted-zen@garagescript.org>
2019-06-01 17:17:31 -0700

    4 update para inside callback

--- js2/4.js
+++ js2/4.js
@@ -15,8 +15,14 @@
  * @returns {function}
  */
 
-const solution = (a, cb) => {
-  return () => {}
+const solution = (a, cb, i = 0) => {
+  return () => {
+    i += 1
+    console.log(i)
+    if (i === a.length + 1) i = 1
+    // i = (i - 1) % a.length
+    return cb(a[i - 1])
+  }
 }
 
 module.exports = {
