dbee5133abebd4af49eab524b1994939d2b65951
Carlos Green <zonxa14@gmail.com>
2019-06-08 17:09:00 -0700

    changes

--- js1/12.js
+++ js1/12.js
@@ -1,9 +1,11 @@
 /**
  * Write a function called solution that
- *   takes in 2 parameters, a number and a function,
- * Solution should execute the input function (which returns a number)
- *   after input number of milliseconds, then execute the input function
- *   again after waiting the returned number of milliseconds.
+ * takes in 2 parameters, a number and a function,
+ * Solution should execute the input function
+ * (which returns a number)
+ * after input number of milliseconds,
+ * then execute the input function
+ * again after waiting the returned number of milliseconds.
  * @param {number} a
  * @param {function} fun
  * @returns null
@@ -11,11 +13,6 @@
 
 const solution = (a, fun) => {
   let num = fun();
-console.log("num =", num)
-//  setTimeout(fun, a)
-console.log("a =", a)
-//  setTimeout(fun, num)
-console.log("fun = ()", fun())
 
   setTimeout(function () {
     setTimeout(fun, a)
