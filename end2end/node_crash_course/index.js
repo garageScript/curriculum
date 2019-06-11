23c5e44730277056ca53230d4912b0775749910b
Carlos Green <zonxa14@gmail.com>
2019-06-11 16:12:11 -0700

    nodeJS

--- end2end/node_crash_course/index.js
+++ end2end/node_crash_course/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+/*  if (req.url === '/') {
     fs.readFile(
     path.join(__dirname, 'public', 'index.html'), 
     (err, content) => {
@@ -22,6 +22,19 @@ const server = http.createServer((req, res) => {
     res.end(content)
   })
  }
+  if (req.url === '/api/users') {
+    const users = [
+    { name: 'Bob Smith', age: 40 },
+    { name: 'John Doe', age: 30 } 
+    ]
+  res.writeHead(200, { 'Content-Type': 'application/json' })	
+  res.end(JSON.stringify(users))
+ }
+*/
+
+// Build file path
+let filePath = path.join()
+
 })
 
 const PORT = process.env.PORT || 5000;
