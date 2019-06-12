da23afa3edf9dd42df53a2d2ab56f736623cb573
Carlos Green <zonxa14@gmail.com>
2019-06-12 12:27:29 -0700

    heroku deplyoment

--- end2end/node_crash_course/index.js
+++ end2end/node_crash_course/index.js
@@ -33,8 +33,58 @@ const server = http.createServer((req, res) => {
 */
 
 // Build file path
-let filePath = path.join()
+let filePath = path.join(
+  __dirname,
+  'public',
+  req.url === '/' ? 'index.html' : req.url
+);
+// Extension of file
+  let extname = path.extname(filePath)
 
+// Initial content type
+  let contentType = 'text/html'
+
+// Check ext and set content type
+switch(extname) {
+  case '.js':
+    contentType = 'text/javascript';
+    break;
+  case '.css':	
+    contentType = 'text/css';
+    break;
+  case '.json':		  
+    contentType = 'application/json';
+    break;
+  case '.png':
+    contentType = 'image/png';
+    break;
+  case '.jpg':
+    contentType = 'image/jpg';
+    break;	
+  }
+
+// Read file
+fs.readFile(filePath, (err, content) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+    // Page not found or 404
+    fs.readFile(path.join(__dirname, 'public', '404.html'), 
+    (err, content) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf8')	 
+    }) 
+    } else {
+    // Some server error or 500
+    res.writeHead(500)
+    res.end(`Server Error: ${err.code}`);	    
+    }
+    } else {
+    // Sucessful response or 200
+    res.writeHead(200, {'Content-Type': contentType });	  
+    res.end(content, 'utf8')	    
+    }
+
+  })
 })
 
 const PORT = process.env.PORT || 5000;
