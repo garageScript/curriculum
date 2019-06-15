28e3d2cdba261da1988ee41b52fab511401e47e2
Carlos Green <zonxa14@gmail.com>
2019-06-15 11:22:44 -0700

    leetcode

--- leetcode/easy/617.js
+++ leetcode/easy/617.js
@@ -3,15 +3,11 @@
 /*
 Given two binary trees and imagine that when you put one of them to cover the other,
 some nodes of the two trees are overlapped while the others are not.
-
 You need to merge them into a new binary tree.
 The merge rule is that if two nodes overlap,
 then sum node values up as the new value of the merged node.
-
 Otherwise, the NOT null node will be used as the node of new tree.
-
 Example 1:
-
 Input:
 	Tree 1                     Tree 2
           1                         2
@@ -19,7 +15,6 @@ Input:
         3   2                     1   3
        /                           \   \
       5                             4   7
-
 Output:
 Merged tree:
 	     3
@@ -27,8 +22,6 @@ Merged tree:
 	   4   5
 	  / \   \
 	 5   4   7
-
-
 Note: The merging process must start from the root nodes of both trees.
 */
 
@@ -58,12 +51,13 @@ f.right = h;
 g.right = j;
 
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+* Definition for a binary tree node.
+
+* function TreeNode(val) {
+*   this.val = val;
+*   this.left = this.right = null;
+* }
+**/
 
 /**
  * @param {TreeNode} t1
@@ -75,8 +69,9 @@ var mergeTrees = function(t1, t2) {
 };
 
 console.log(mergeTrees(a, e))
-console.log(`3 \n
-	    / \ \n
-	   4   5 \n
-	  / \   \ \n
-	 5   4   7`)
+// Output
+/*  3
+   / \
+  4   5
+ / \   \
+5   4   7 */
