dcd733691943856fd43a6915671ddc57f4a4bdf5
Carlos Green <zonxa14@gmail.com>
2019-07-27 15:22:50 -0700

    changs

--- js2/5.js
+++ js2/5.js
@@ -4,9 +4,18 @@
  *   and calls every function input milliseconds later
  * @param {array} a
  * @param {number} t
- */
 
-const solution = (a, t) => {
+const func = () => {
+  console.log('hello')
+}
+solution([func,func,func], 300) 
+// console.log('hello') will be run 3 times, after 300ms
+*/
+
+const solution = (a, t, i = 0) => {
+  if (i === a.length) return;
+  setTimeout(a[i], t)
+  return solution(a, t, i + 1)
 }
 
 module.exports = {
--- js2/6.js
+++ js2/6.js
@@ -4,10 +4,34 @@
  *     and calls each function input milliseconds after another
  * @param {array} a
  * @param {number} t
- */
 
-const solution = (a, t) => {
+ const func = () => {
+  console.log('hello')
 }
+
+solution([func,func,func], 300) 
+// console.log('hello') will be run after 300ms
+// console.log('hello') will be run after 300ms
+// console.log('hello') will be run after 300ms
+*/
+
+//const func = () => {
+//  console.log('hello')
+//}
+
+//const problem = ([func, func, func], 300)
+
+const solution = (a, t, i = 0) => {
+  if (i === a.length) return
+  setTimeout(a[i], t)
+  return solution(a, t, i + 1)
+}
+
+//console.log(solution(problem))
+
+/*const solution = (a, t) => {
+
+}*/
 
 module.exports = {
   solution
