29e3b21702139db6ad35d01414f1d0468ccc952f
ted-zen <teddylhl@gmail.com>
2019-09-07 14:22:17 -0700

    with recursion

--- js3/11.js
+++ js3/11.js
@@ -8,12 +8,14 @@
 // You can use helper functions like:
 //     Object.keys, Object.values, or Object.entries
 const solution = () => {
-  Object.prototype.gsForEach = function () {
+  Object.prototype.gsForEach = function (cb, i = 0, arr = Object.entries(this)) {
+    if (i === arr.length) return
+    cb(arr[i][0], arr[i][1])
+    return this.gsForEach(cb, i + 1)
     // To get the actual object, use this
-  };
-};
+  }
+}
 
 module.exports = {
-  solution,
-};
-
+  solution
+}
