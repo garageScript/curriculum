96bd8f8673b4f659f9a96b93852f8a3ab18e3b0f
ted-zen <teddylhl@gmail.com>
2019-09-07 16:04:28 -0700

    wrtieFile in callback

--- js4/2.js
+++ js4/2.js
@@ -2,4 +2,18 @@
 // Write a script that creates a file ~/users.txt that contains all the users inside the /home folder that is not you.
 
 // fs.writeFile('fileName', 'fileData', () => {});
+const fs = require('fs')
 
+fs.readdir('/home', (err, files) => {
+  if (err) throw err
+
+  const data = files.reduce((data, e) => {
+    if (e !== 'ted-zen') return data + '\n' + e
+    return data
+  }, '')
+
+  fs.writeFile(__dirname + '/user.data', data, (err) => {
+    if (err) throw err
+    console.log('write success')
+  })
+})
