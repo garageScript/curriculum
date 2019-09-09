9b13ad18ff62612312a552461ef412b83938208c
Daniel Thalhuber <dev@uhf.li>
2019-09-08 23:02:23 -0700

    js07c11: fix test

--- js7/__tests__/11.js
+++ js7/__tests__/11.js
@@ -3,8 +3,7 @@ const solution = require('../11');
 solution();
 
 describe('resolveAll', () => {
-  it( 'should fulfill with array of fulfilled promise values, in order'
-  , function(done) {
+  it('should fulfill with array of fulfilled promise values, in order', function(done) {
     const accs = [];
     const es = [];
     const is = [];
@@ -12,33 +11,36 @@ describe('resolveAll', () => {
       new Promise((res, rej) => res('c')),
       new Promise((res, rej) => res('a')),
       new Promise((res, rej) => res('b')),
-    ].asyncReduce((acc, e, i) => {
-      accs.push(acc);
-      es.push(e);
-      is.push(i);
-      return acc + e;
-    }, 'z').then( (result) => {
-      if (JSON.stringify(accs) !== JSON.stringify(['z', 'zc', 'zca'])) {
-        return done(new Error('accumulator is not passed correctly'));
-      }
-      if (JSON.stringify(es) !== JSON.stringify(['c', 'a', 'b'])) {
-        return done(new Error('element is not passed correctly'));
-      }
-      if (JSON.stringify(accs) !== JSON.stringify([0, 1, 2])) {
-        return done(new Error('index is not passed correctly'));
-      }
-      if (data !== 'zcab') {
-        return done(new Error('accumulator did not fire correctly'));
-      }
-      return done();
-    });
+    ]
+      .asyncReduce((acc, e, i) => {
+        accs.push(acc);
+        es.push(e);
+        is.push(i);
+        return acc + e;
+      }, 'z')
+      .then((result) => {
+        if (JSON.stringify(accs) !== JSON.stringify(['z', 'zc', 'zca'])) {
+          return done(new Error('accumulator is not passed correctly'));
+        }
+        if (JSON.stringify(es) !== JSON.stringify(['c', 'a', 'b'])) {
+          return done(new Error('element is not passed correctly'));
+        }
+        if (JSON.stringify(is) !== JSON.stringify([0, 1, 2])) {
+          return done(new Error('index is not passed correctly'));
+        }
+        if (result !== 'zcab') {
+          return done(new Error('accumulator did not fire correctly'));
+        }
+        return done();
+      });
   });
   it('should reject with first rejection value', () => {
     const array = [
       new Promise((res, rej) => rej('1st rejection')),
       new Promise((res, rej) => rej('2nd rejection')),
     ];
-    return array.asyncReduce((acc, cv) => acc += cv, 0)
+    return array
+      .asyncReduce((acc, cv) => (acc += cv), 0)
       .catch((err) => expect(err).to.equal('1st rejection'));
   });
 });
