9c0d4eabb71ec5c8b3a26f9a12d5ea8cc52f69df
ted-zen <teddylhl@gmail.com>
2019-09-10 21:44:17 -0700

    express server

--- js4/3.js
+++ js4/3.js
@@ -1,8 +1,19 @@
 // Creating Your First Site - Express Yourself
 // Create an express server that sends back <h1>, <input>, and <button> tag. When you visit url/hello. Make the h1 tag red using (CSS).
-
 // const express = require('express');
 // const app = express();
 // app.get('url', handler);
 // app.listen(PORT)
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 9716
 
+app.get('/hello', function (req, res) {
+  res.send(`
+    <h1 style = "color :tomato;"> hi </h1>
+    `)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`)
+})
