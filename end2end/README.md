c524970c5a4e933677c5a77e481dd372ced36400
Carlos Green <zonxa14@gmail.com>
2019-09-25 18:06:09 -0700

    end2end chatroom

--- end2end/README.md
+++
@@ -1,6 +0,0 @@
-local node and express
-that isn't ssh username@c0d3.com
-
-pwd -> print working directory
-/Users/carlosgreen/Desktop/Play/GarageScript/end2end
-
--- end2end/public/sevenOne.js
+++
@@ -1,7 +0,0 @@
-const user = document.getElementById("user")
-const submit = document.getElementById("submited")
-
-submit.onclick = () => {
-  res.cookie('name', user)
-}
-
