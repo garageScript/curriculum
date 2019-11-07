0b2e2241d09494a3cf5c20fc491947f269453d50
pvalderama <34151463+pvalderama@users.noreply.github.com>
2019-11-07 14:25:20 -0800

    js3 6 and tests 6.js

--- js3/__tests__/6.js
+++ js3/__tests__/6.js
@@ -1,26 +1,17 @@
-const expect = require('chai').expect;
 const solution = require('../6').solution;
 
-describe('filter an object if the condition is true', () => {
-   it('should return base case', () =>{
-    const obja = {};
-
-    const result = solution(obja,(k,v)=>{return v.length>3});
-    expect(result).to.deep.equal({});
+describe('takes an array, returns array of duplicate numbers', () => {
+   it('should return []', () =>{
+    const result = solution([1, 2, 3, 4, 5]);
+    expect(result).toEqual([]);
   });
- 
-  it('should return {5:blah blah,zolo:4thech} for input {5:blah blah, name:ho, zolo:4thech', () =>{
-    const obja = {5:'blah blah',name:'ho',zolo:'4thech'};
-
-    const result = solution(obja,(k,v)=>{return v.length>3});
-    expect(result).to.deep.equal({5:'blah blah',zolo:'4thech'});
+  it('should return [2, 3, 4]', () => {
+    const result = solution([1, 2, 3, 2, 2, 3, 4, 4, 3]);
+    expect(result).toEqual([2, 3, 4]);
   });
-  it('should return {5:blah blah} for input {5:blah blah, name:ho, zolo:4thech', () =>{
-    const obja = {5:'blah blah',name:'ho',zolo:'4thech'};
-
-    const result = solution(obja,(k,v)=>{return v.length>7});
-    expect(result).to.deep.equal({5:'blah blah'});
+  it('should return [23, 12, 45]', () => {
+    const result = solution([23, 45, 12, 23, 44, 12, 45]);
+    expect(result).toEqual([23, 12, 45]);
   });
-
 });
 
