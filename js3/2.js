6cd4dde8a4c6ee7117d5ce418e73bd4b0f81c8f3
pvalderama <34151463+pvalderama@users.noreply.github.com>
2019-11-07 10:33:10 -0800

    js3 2.js/test 2.js

--- js3/2.js
+++ js3/2.js
@@ -1,11 +1,19 @@
 /**
- * mostCommon - takes in array, return most common number
- * @param {array} arr
- * @return {number}
+ * takes in 2 integers, create 2d array of objects. First integer represents how many nested arrays within the array. Second integer represents how many objects within each array. 
+ * solution(4,2)
+ * returns: 
+ * [
+    [{x: 0, y: 0}, {x: 1, col: 0}],
+    [{x: 0, y: 1}, {x: 1, col: 1}],
+    [{x: 0, y: 2}, {x: 1, col: 2}],
+    [{x: 0, y: 3}, {x: 1, col: 3}],
+  ]
+ * @param {integer} num1 {integer} num2
+ * @return {array} arr
  */
 
-const solution = (arr)=>{
-  return 0;
+const solution = (num1, num2)=>{
+  return arr;
 };
 
 module.exports = {
