444549c94b26243282edd95d5646bb0c34532467
pvalderama <34151463+pvalderama@users.noreply.github.com>
2019-11-07 13:27:59 -0800

    4.js and test 4.js

--- js3/4.js
+++ js3/4.js
@@ -1,6 +1,5 @@
 /*
- * xpair - takes in array and a number, return true if any pairs add
- * up to the number.
+ * 2sum: write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number. (No duplicates)
  * @param {array} a
  * @param {number} b
  * @returns {boolean}
