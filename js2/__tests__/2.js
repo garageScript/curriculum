adaccf4bad2186675b10410a53504f846b4f4017
root <root@garagescript.org>
2019-11-06 20:44:39 -0800

    fix js2 tests

--- js2/__tests__/2.js
+++ js2/__tests__/2.js
@@ -7,24 +7,24 @@ describe('Generate array', () => {
     const result = solution((e) => {
       return true
     })
-    expect(result).to.deepEqual([])
+    expect(result).toEqual([])
   })
   it('should generate 1d array with 1 values', () => {
     const result = solution((e) => {
       return e > 0
     })
-    expect(result).to.deepEqual([0])
+    expect(result).toEqual([0])
   })
   it('should generate 1d array with 5 values', () => {
     const result = solution((e) => {
       return (e % 7 === 0) && e !== 0
     })
-    expect(result).to.deepEqual([0, 1, 2, 3, 4, 5, 6])
+    expect(result).toEqual([0, 1, 2, 3, 4, 5, 6])
   })
   it('should generate 1d array with 10 values', () => {
     const result = solution((e) => {
       return e > 9
     })
-    expect(result).to.deepEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    expect(result).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
   })
 })
