18c39f88ca73b72c845fbe39f703d738c8de5c1f
pvalderama <34151463+pvalderama@users.noreply.github.com>
2019-11-07 19:07:52 -0800

    5.js and tests 5.js

--- js3/5.js
+++ js3/5.js
@@ -2,11 +2,11 @@
  * Given object of key: string values and an object of key: function values, call the functions in 2nd object, using the values in 1st object as function params
  * @param {object} a
  * @param {object} b
- * @return {nothing}
+ * @return {object} c
  **/
 
 const solution = (a,b)=>{
-  return;
+  return c;
 }
 module.exports = {
   solution
--- js3/__tests__/5.js
+++ js3/__tests__/5.js
@@ -1,17 +1,17 @@
 const solution = require('../5').solution;
 
-describe('using the same keys, call functions in 2nd object using values from 1st object as params', () => {
-  it('should return ""', () => {
+describe('call function in 2nd object (if possible) using the corresponding values in 1st object as function params, return new object', () => {
+  it('should return if object is empty ', () => {
     const result = solution({}, {});
-    expect(result).toEqual("");
+    expect(result).toEqual();
   })
-  it('should return "johnnyboy"', () => {
-    const result = solution({'name': 'johnny'}, {'name': (e) => {console.log(e + 'boy')}, 'lastname': (e) => {console.log(e + 'girl')}});
-    expect(result).toEqual('johnnyboy');
+  it('should return new object', () => {
+    const result = solution({"name": "pikachu", "age": "59", "power": 10, "color": "red"}, {"name": (e) => {return "Raichu"}, "power": (num) => {1000}});
+    expect(result).toEqual({"name": "Raichu", "age": "59", "power": 1000, "color": "red"})
   });
   it('should return ', () => {
-    const result = solution({'shoes': 'new balances'}, {'pants': (e) => {console.log(e + 'khakis')}, 'shoes': (e) => {console.log(e + ' are comfy')}});
-    expect(result).toEqual('new balances are comfy');
+    const result = solution({"name": "khaleesi", "age": 25, "power": "mother of dragons", "weakness": "john snow"}, {"weakness": (e) => {return "she has none"}, "power": (e) => {return "fire proof"}});
+    expect(result).toEqual({"name": "khaleesi", "age": 25, "power": "fire proof", "weakness": "she has none"});
   });
 });
 
