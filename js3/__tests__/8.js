1bb0247f35a1b39e22376c4c9e110bfc5b27f76e
Paul Valderama <34151463+pvalderama@users.noreply.github.com>
2019-11-08 23:09:15 -0800

    JS3/ 8.js and test 8.js (#288)
    * js3 2.js/test 2.js
    
    * js3 tests 2.js
    
    * base cases test 2.js
    
    * 8.js and test 8.js
    
    * js3 8.js test

--- js3/__tests__/8.js
+++ js3/__tests__/8.js
@@ -1,55 +1,32 @@
-const expect = require('chai').expect;
 const solution = require('../8').solution;
 
-describe('circular linked list', () => {
-   it('should return false for one node that is not circular', () =>{
-    const obja = {v: 5};
+describe('It should call each function value of the object millieseconds after one another', () => {
+    it('It should not call the function if the object is empty', (done) => {
+      let sum = 0;
+      const a = {};
+      solution(a, 200);
+      expec(sum).toEqual(0);
+    });
+   it('Sum will be 0, "one", "onetwo" after  ', (done) =>{
+    let sum = 0;
+    const a = {
+      'one': (d) => sum += d,
+      'two': (c) => sub += e,
+    };
+    solution(a, 100);
 
-    const result = solution(obja);
-    expect(result).to.equal(false);
-  });
+    setTimeout(() => {
+      expect(sum).toEqual(0);
+    }, 50);
 
-   it('should return true for one node that is circular', () =>{
-    const obja = {v: 5};
-     obja.next = obja;
+    setTimeout(() => {
+      expect(sum).toEqual('0one');
+    }, 150);
 
-    const result = solution(obja);
-    expect(result).to.equal(true);
-  });
-
-  it('should return false for non-circular linked list with 3 nodes', () =>{
-    const obj1 = {v: 1};
-    const obj2 = {v: 2};
-    const obj3 = {v: 3};
-    obj1.next = obj2;
-    obj2.next = obj3;
-
-    const result = solution(obj1);
-    expect(result).to.equal(false);
-  });
-
-  it('should return true for circular linked list with 3 nodes', () =>{
-    const obj1 = {v: 1};
-    const obj2 = {v: 2};
-    const obj3 = {v: 3};
-    obj1.next = obj2;
-    obj2.next = obj3;
-    obj3.next = obj2;
-
-    const result = solution(obj1);
-    expect(result).to.equal(true);
-  });
-
-  it('should return true for circular linked list back to first node', () =>{
-    const obj1 = {v: 1};
-    const obj2 = {v: 2};
-    const obj3 = {v: 3};
-    obj1.next = obj2;
-    obj2.next = obj3;
-    obj3.next = obj1;
-
-    const result = solution(obj1);
-    expect(result).to.equal(true);
+    setTimeout(() => {
+      expect(sum).toEqual('0onetwo');
+      done();
+    }, 250);
   });
 });
 
