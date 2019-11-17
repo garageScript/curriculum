db857bd59f0d8c655f8fdfc7ff674e7276eb70d5
ted-zen <teddylhl@gmail.com>
2019-11-16 19:27:30 -0800

    c0d3 style and dir path update

--- js4/4.js
+++ js4/4.js
@@ -1,18 +1,20 @@
 const fs = require('fs')
 const express = require('express')
 const app = express()
+const PORTNUM = process.env.PORT || 9999
 
 fs.readdir('/home', (err, files) => {
   if (err) throw err
   const data = files.reduce((data, e) => {
-    if (e !== 'ted-zen') return `${data}<br/>${e}>`
+    if (e !== 'ted-zen') return `${data} <br/> ${e}`
     return data
   }, '')
-  app.get('/', function (req, res) {
-    res.send(`<h3 style="color:blue"> ${data} </h3>`)
+  app.get('/users', function (req, res) {
+    res.send(`
+          <h3 style = "color:blue"> ${data}</h3>
+           `)
   })
-  const port = process.env.PORT || 9999
-  app.listen(port, () => {
-    console.log(`Listening on port: ${port}`)
+  app.listen(PORTNUM, () => {
+    console.log(`Listening on port: ${PORTNUM}`)
   })
 })
