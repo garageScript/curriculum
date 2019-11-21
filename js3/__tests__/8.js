b6949a37306b21e2ba9b6556271e297f3de2e5d5
Song Zheng <coldangelfire311@gmail.com>
2019-11-20 23:41:30 -0800

    fix test 8 (#293)

--- js3/__tests__/8.js
+++ js3/__tests__/8.js
@@ -1,32 +1,32 @@
-const solution = require('../8').solution;
+/* global describe it expect */
+const solution = require('../8').solution
 
 describe('It should call each function value of the object millieseconds after one another', () => {
-    it('It should not call the function if the object is empty', (done) => {
-      let sum = 0;
-      const a = {};
-      solution(a, 200);
-      expec(sum).toEqual(0);
-    });
-   it('Sum will be 0, "one", "onetwo" after  ', (done) =>{
-    let sum = 0;
+  it('It should not call the function if the object is empty', (done) => {
+    let sum = 0
+    const a = {}
+    solution(a, 50)
+    expect(sum).toEqual(0)
+    setTimeout(() => {
+      expect(sum).toEqual(0)
+      done()
+    }, 55)
+  })
+  it('Sum will be 0, "one", "onetwo" after  ', (done) => {
+    let sum = 0
     const a = {
       'one': (d) => sum += d,
-      'two': (c) => sub += e,
-    };
-    solution(a, 100);
+      'two': (c) => sum += c
+    }
+    solution(a, 100)
 
     setTimeout(() => {
-      expect(sum).toEqual(0);
-    }, 50);
+      expect(sum).toEqual('0one')
+    }, 50)
 
     setTimeout(() => {
-      expect(sum).toEqual('0one');
-    }, 150);
-
-    setTimeout(() => {
-      expect(sum).toEqual('0onetwo');
-      done();
-    }, 250);
-  });
-});
-
+      expect(sum).toEqual('0onetwo')
+      done()
+    }, 150)
+  })
+})
