f3c6b869ef78bf660449fcc0506d7acdd9184259
Song Zheng <song@zheng.club>
2019-11-21 16:55:30 -0800

    fix tests

--- js3/__tests__/1.js
+++ js3/__tests__/1.js
@@ -1,20 +1,22 @@
-let solution = require('../1').solution;
+/* global describe it expect */
+
+let solution = require('../1').solution
 
 describe('given array of strings(keys) and object, return an array of values', () => {
   it('should return ["arrogant", "naive"]', () => {
-    const result = solution(['ironman', 'spiderman'], {ironman: 'arrogant', blackpanther: 'brave', spiderman: 'naive'});
-    expect(result).toEqual(['arrogant', 'naive']);
-  });
+    const result = solution(['ironman', 'spiderman'], { ironman: 'arrogant', blackpanther: 'brave', spiderman: 'naive' })
+    expect(result.sort()).toEqual(['arrogant', 'naive'].sort())
+  })
   it('should return ["goat", "mamba"]', () => {
-    const result = solution(['jordan', 'kobe'], {jordan: 'goat', kobe: 'mamba', lebron: 'pretender'});
-    expect(result).toEqual(['goat', 'mamba']);
-  });
+    const result = solution(['jordan', 'kobe'], { jordan: 'goat', kobe: 'mamba', lebron: 'pretender' })
+    expect(result.sort()).toEqual(['goat', 'mamba'].sort())
+  })
   it('should return ["greatest shooter of all time", "Greek freak"]', () => {
-    const result = solution(['curry', 'giannis'], {curry: 'greatest shooter of all time', harden: 'flopper', giannis: 'Greek freak'});
-    expect(result).toEqual(['greatest shooter of all time', 'Greek freak']);
-  });
+    const result = solution(['curry', 'giannis'], { curry: 'greatest shooter of all time', harden: 'flopper', giannis: 'Greek freak' })
+    expect(result.sort()).toEqual(['greatest shooter of all time', 'Greek freak'].sort())
+  })
   it('should return ["pancakes", "steak"]', () => {
-    const result = solution(['breakfast', 'dinner'], {breakfast: 'pancakes', lunch: 'sandwhich', dinner: 'steak'});
-    expect(result).toEqual(['pancakes', 'steak']);
-  });
-});
+    const result = solution(['breakfast', 'dinner'], { breakfast: 'pancakes', lunch: 'sandwhich', dinner: 'steak' })
+    expect(result.sort()).toEqual(['pancakes', 'steak'].sort())
+  })
+})
--- js3/__tests__/7.js
+++ js3/__tests__/7.js
@@ -1,27 +1,28 @@
-const solution = require('../7').solution;
+/* global describe it expect */
+const solution = require('../7').solution
 
 solution()
 
 describe('map', () => {
-   it('should not run callback on empty object, return empty arr', () =>{
-    const info = {};
+  it('should not run callback on empty object, return empty arr', () => {
+    const info = {}
     const result = info.map((key, value, i) => {
-      return key + i + value;
-    });
+      return key + i + value
+    })
     expect(result).toEqual([])
-  });
+  })
   it('should run callback on object, return array ', () => {
-    const info = {ironman: 'arrogant', spiderman: 'naive', hulk: 'strong',};
+    const info = { ironman: 'arrogant', spiderman: 'naive', hulk: 'strong' }
     const result = info.map((key, value, i) => {
-      return key + i + value;
-    });
-    expect(result).toEqual(['ironman0arrogant', 'spiderman1naive', 'hulk2strong']);
-  });
+      return key + i + value
+    })
+    expect(result.sort()).toEqual(['ironman0arrogant', 'spiderman1naive', 'hulk2strong'].sort())
+  })
   it('should run callback on object, return array ', () => {
-    const info = {jet: 'superfast', yacht: 'luxurious', car: 'practical'};
+    const info = { jet: 'superfast', yacht: 'luxurious', car: 'practical' }
     const result = info.map((key, value, i) => {
-      return key + i + value;
-    });
-    expect(result).toEqual(['jet0superfast', 'yacht1luxurious', 'car2practical']);
-  });
-});
+      return key + i + value
+    })
+    expect(result.sort()).toEqual(['jet0superfast', 'yacht1luxurious', 'car2practical'].sort())
+  })
+})
