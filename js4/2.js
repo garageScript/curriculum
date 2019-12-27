bb6caebf27d77317d7f5d6714b9c66a8d81e21a2
Paul Valderama <34151463+pvalderama@users.noreply.github.com>
2019-12-27 15:02:42 -0800

    Delete JS4 files, create html files that correspond to JS4 challenges (#304)
    * remove js3/11
    
    * delete JS4 files add Submit folder and README.md
    
    * js4 challenge fies
    
    * Deleted JS4 files, update README.md

--- js4/2.js
+++
@@ -1,5 +0,0 @@
-// Getting To Know Your Computer, pt. 2 - Write All Except You
-// Write a script that creates a file ~/users.txt that contains all the users inside the /home folder that is not you.
-
-// fs.writeFile('fileName', 'fileData', () => {});
-
---
+++ js4/README.md
@@ -0,0 +1,4 @@
+Create each HTML file for the JS4 challenges. Place each challenge into the JS4 folder.
+For example, '1.html' for challenge 1, '2.html' for challenge 2, etc. 
+
+Make sure you organize your code to make it readable for the reviewer.
