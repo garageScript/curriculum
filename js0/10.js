bd9681cf026fba9ae768856fbd8f661faf735b6c
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 09:12:41 -0800

    Update 10.js

--- js0/10.js
+++ js0/10.js
@@ -2,12 +2,12 @@
  * Write a function called solution that
  * Takes in 2 functions, returns
  *   the sum of the result of calling the 2 functions
- * @param {function} a
- * @param {function} b
+ * @param {function} func1
+ * @param {function} func2
  * @returns {number}
  */
 
-const solution = (a, b) => {
+const solution = (func1, func2) => {
   return 0
 }
 
