d910964a0e139350b3b1642ff20e98ed5848cf12
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 09:23:49 -0800

    Update 4.js

--- js2/4.js
+++ js2/4.js
@@ -10,12 +10,12 @@
  * When the returned function is called for the third time,
  *     the input function will be called with the third element of the array.
  * ... loop back to first element when it reaches the end
- * @param {array} a
+ * @param {array} arr
  * @param {function} cb
  * @returns {function}
  */
 
-const solution = (a, cb) => {
+const solution = (arr, cb) => {
   return () => {}
 }
 
