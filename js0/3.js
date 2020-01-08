0a876448f886384f68bdeee4b53d860f73a383f8
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 09:06:27 -0800

    Update 3.js

--- js0/3.js
+++ js0/3.js
@@ -1,11 +1,11 @@
 /**
  * Write a function called solution that
  * takes in a number and returns true if that number is greater than 5, false otherwise
- * @param {number} a
+ * @param {number} num
  * @returns {boolean}
  */
 
-const solution = (a) => {
+const solution = (num) => {
   return true
 }
 
