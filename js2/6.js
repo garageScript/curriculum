c17f7ed658bd2041539360fe74fd36d8c4eba13d
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 23:28:11 -0800

    Update 6.js (#326)

--- js2/6.js
+++ js2/6.js
@@ -2,11 +2,11 @@
  * Write a function called solution that
  *   that takes in an array of functions and a number,
  *     and calls each function input milliseconds after another
- * @param {array} a
- * @param {number} t
+ * @param {array} arr
+ * @param {number} time
  */
 
-const solution = (a, t) => {
+const solution = (arr, time) => {
 }
 
 module.exports = {
