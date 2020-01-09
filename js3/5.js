c7b783250a1a0425ffb2ac7758924cdd1a73dd45
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 23:27:39 -0800

    Update 5.js (#328)

--- js3/5.js
+++ js3/5.js
@@ -1,11 +1,11 @@
 /*
  * Given object of key: string values and an object of key: function values, call the functions in 2nd object, using the values in 1st object as function params
- * @param {object} a
- * @param {object} b
- * @return {object} c
+ * @param {object} obj1
+ * @param {object} obj2
+ * @return {object}
  **/
 
-const solution = (a, b) => {
+const solution = (obj1, obj2) => {
   return {}
 }
 module.exports = {
