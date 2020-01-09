966d811dd6973bf3e9cfe5912db3e961d1e9fa17
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 23:35:17 -0800

    Update 8.js (#313)

--- js0/8.js
+++ js0/8.js
@@ -2,12 +2,12 @@
  * Write a function called solution that
  * Takes in 2 numbers and returns a function
  * The returned function returns the sum when it is called
- * @param {number} a
- * @param {number} b
+ * @param {number} num1
+ * @param {number} num2
  * @returns {function}
  */
 
-const solution = (a, b) => {
+const solution = (num1, num2) => {
   return () => {
   }
 }
