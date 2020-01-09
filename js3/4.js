5403ef96a5fc15ab5f4de90f4bbbeb65a977d422
Carlos Green <30880055+greenc123@users.noreply.github.com>
2020-01-08 23:28:38 -0800

    Update 4.js (#327)

--- js3/4.js
+++ js3/4.js
@@ -1,11 +1,11 @@
 /*
  * 2sum: write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number. (No duplicates)
- * @param {array} a
- * @param {number} b
+ * @param {array} arr
+ * @param {number} num
  * @returns {boolean}
  */
 
-const solution = (a, b) => {
+const solution = (arr, num) => {
   return true
 }
 
