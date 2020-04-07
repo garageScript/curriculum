8d9b2655d3727ba87386524c64dc8bfedc7bdb15
Daniel Thalhuber <dev@uhf.li>
2020-04-07 16:19:48 -0700

    js01c06: test: refactor, add tests for additional non-primes
    - Add non-primes less than 2 to align with challenge description
    - Refactor to remove repetition

--- js1/__tests__/6.js
+++ js1/__tests__/6.js
@@ -1,38 +1,22 @@
-/* global describe it expect */
+const solution = require('../6').solution;
 
-const solution = require('../6').solution
+const primes = [2, 3, 5, 7, 11, 13, 1223];
+const nonPrimes = [-200, -1, 0, 1, 4, 6, 8, 9, 10, 12, 1226];
 
-describe('check if number x is prime', () => {
-  it('2 should be prime', () => {
-    const result = solution(2)
-    expect(result).toEqual(true)
-  })
-  it('3 should be prime', () => {
-    const result = solution(3)
-    expect(result).toEqual(true)
-  })
-  it('1 should not be prime', () => {
-    const result = solution(1)
-    expect(result).toEqual(false)
-  })
-  it('7 should be prime', () => {
-    const result = solution(7)
-    expect(result).toEqual(true)
-  })
-  it('9 should not be prime', () => {
-    const result = solution(9)
-    expect(result).toEqual(false)
-  })
-  it('12 should not be prime', () => {
-    const result = solution(12)
-    expect(result).toEqual(false)
-  })
-  it('1223 should be prime', () => {
-    const result = solution(1223)
-    expect(result).toEqual(true)
-  })
-  it('1226 should not be prime', () => {
-    const result = solution(1226)
-    expect(result).toEqual(false)
-  })
-})
+describe('isPrime', () => {
+  describe('correctly identifies prime numbers', () => {
+    primes.forEach((prime) => {
+      test(`identifies ${prime} as a prime number`, () => {
+        expect(solution(prime)).toEqual(true);
+      });
+    });
+  });
+
+  describe('correctly identifies non-prime numbers', () => {
+    nonPrimes.forEach((nonPrime) => {
+      test(`identifies ${nonPrime} as a non-prime number`, () => {
+        expect(solution(nonPrime)).toEqual(false);
+      });
+    });
+  });
+});
