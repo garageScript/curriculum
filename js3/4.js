afb50492a3d5a71043e457168ee3c1961130d5b0
Sly <khmedj@gmail.com>
2020-09-14 00:21:10 +0100

    Update js3/4.js problem description. (#355)
    Updated the problem description to match the one on the website.

--- js3/4.js
+++ js3/4.js
@@ -1,5 +1,6 @@
 /*
- * 2sum: write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number. (No duplicates)
+ * 2sum: write a function that takes in an array of numbers and a number, and returns true if any pairs add up to the number.
+ * (The numbers in the array is not unique, meaning there may be duplicate numbers)
  * @param {array} arr
  * @param {number} num
  * @returns {boolean}
