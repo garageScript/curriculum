9687a13612616d32ed1188beec19009f911f7695
Bryan Santos <brymsantos@gmail.com>
2021-11-06 15:37:10 -0700

    solves js2 problem 2

--- js2/2.js
+++ js2/2.js
@@ -5,11 +5,15 @@
  * As long as the input function returns false,
  *   array keeps growing with the correspending index value
  * @param {function} fun
+ * @param {number} i
+ * @param {array} result
  * @returns {array}
  */
 
-const solution = (fun) => {
-  return []
+const solution = (fun, i=0, result = []) => {
+  if(fun(i)) return result
+  result.push(i)
+  return solution(fun, i+1, result)
 }
 
 module.exports = {
