5e7da5f58c2683e55a8f9865fefa4288f852161b
Bryan Santos <brymsantos@gmail.com>
2021-11-16 17:37:26 -0800

    removed 2nd object from the result array inside the reduce function

--- js3/6.js
+++ js3/6.js
@@ -7,15 +7,14 @@
 const solution = (arr) => {
 
   const answer = arr.reduce((result, e)=>{
-    if(result[0][e] > 0 && !result[1][e]){
-      result[1][e] = true
-      result[2].push(parseInt(e))
+    if(result[0][e] === 1){
+      result[1].push(parseInt(e))
     }
     result[0][e] = (result[0][e] || 0) + 1
     return result
-  },[{},{},[]])
+  },[{},[]])
 
-  return answer[2]
+  return answer[1]
   
 }
 
