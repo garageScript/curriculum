ee0629bbdfb2efc3bef6094a7026ed3db4862d66
XEROling <nhkomaiha@gmail.com>
2022-01-06 13:53:16 +0200

    add missing parameter testing + overall update

--- js3/__tests__/5.js
+++ js3/__tests__/5.js
@@ -1,25 +1,60 @@
 /* globals describe expect it */
-const solution = require('../5').solution
+const solution = require("../5").solution
 
-describe('call function in 2nd object (if possible) using the corresponding values in 1st object as function params, return new object', () => {
+describe("call function in 2nd object (if possible) using the corresponding values in 1st object as function params, return new object", () => {
+	it("should return {} if object is empty ", () => {
+		const result = solution({}, {})
+		expect(result).toEqual({})
+	})
 
-  it('should return if object is empty ', () => {
-    const result = solution({}, {})
-    expect(result).toEqual({})
-  })
+	it("should return {} when obj1 is empty even if obj2 contains entries", () => {
+		const result = solution(
+			{},
+			{
+				white: () => {
+					return "walkers"
+				},
+			}
+		)
+		expect(result).toEqual({})
+	})
 
-  it('should return {}', () => {
-    const result = solution({}, { 'white': () => { return 'walkers' } })
-    expect(result).toEqual({})
-  })
+	it("should return unmodified object if obj1 and obj2 do not match", () => {
+		const result = solution(
+			{
+				codename: "Wall-E",
+				friends: ["Mr. Roach", "Eva"],
+				job: "Save the planet",
+				strength: "He smol but he saved the planet",
+			},
+			{
+				timeline: (e) => e + " (Future)",
+				workHours: (e) => 12 + e,
+			}
+		)
 
-  it('should return new object', () => {
-    const result = solution({ 'name': 'pikachu', 'age': '59', 'power': 10, 'color': 'red' }, { 'name': (e) => { return 'Raichu' }, 'power': (num) => 1000 })
-    expect(result).toEqual({ 'name': 'Raichu', 'age': '59', 'power': 1000, 'color': 'red' })
-  })
+		expect(result).toEqual({
+			codename: "Wall-E",
+			friends: ["Mr. Roach", "Eva"],
+			job: "Save the planet",
+			strength: "He smol but he saved the planet",
+		})
+	})
 
-  it('should return ', () => {
-    const result = solution({ 'name': 'khaleesi', 'age': 25, 'power': 'mother of dragons', 'weakness': 'john snow' }, { 'weakness': (e) => { return 'she has none' }, 'power': (e) => { return 'fire proof' } })
-    expect(result).toEqual({ 'name': 'khaleesi', 'age': 25, 'power': 'fire proof', 'weakness': 'she has none' })
-  })
+	it("should return new object with modified values (strings and numbers must be valid)", () => {
+		const result = solution(
+			{ name: "khaleesi", age: 25, power: "mother of dragons", weakness: "john snow" },
+			{
+				power: (e) => `${e}, burns cities`,
+				weakness: (e) => `${e}, he's fire proof`,
+				age: (e) => e + 5,
+			}
+		)
+		expect(result).toEqual({
+			name: "khaleesi",
+			age: 30,
+			power: "mother of dragons, burns cities",
+			weakness: "john snow, he's fire proof",
+		})
+	})
 })
