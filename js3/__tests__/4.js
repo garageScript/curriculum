a396d6c810ce20554fcbbc3d0549b51384db25ca
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-07-08 11:33:15 +0000

    fix: Add a case to handle skipping first element

--- js3/__tests__/4.js
+++ js3/__tests__/4.js
@@ -3,20 +3,24 @@
 const solution = require('../4').solution
 
 describe('take in target number and array, return true if two numbers in array add up to the target, ', () => {
-  it('should return base case [],0', () => {
+  it('should return base case [], 0', () => {
     const result = solution([], 0)
     expect(result).toEqual(false)
   })
-  it('should return true for input [1,4,3,2],6', () => {
+  it('should return true for input [1,4,3,2], 6', () => {
     const result = solution([1, 4, 3, 2], 6)
     expect(result).toEqual(true)
   })
-  it('should return false for input [-1,-3,-4],-2', () => {
+  it('should return false for input [-1,-3,-4], -2', () => {
     const result = solution([-1, -3, -4], -2)
     expect(result).toEqual(false)
   })
-  it('should return true for input [0,0,0]', () => {
+  it('should return true for input [0,0,0], 0', () => {
     const result = solution([0, 0, 0], 0)
+    expect(result).toEqual(true)
+  })
+  it('should return true for input [1, 2, 3], 3', () => {
+    const result = solution([1, 2, 3], 3)
     expect(result).toEqual(true)
   })
 })
