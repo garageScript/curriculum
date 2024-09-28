ccb9ac76998e0d0375152255b65cdb10a37ff772
Adrian Anin <adrianeanin@gmail.com>
2024-09-28 20:55:19 +0000

    solve problem 5 of js2

--- js2/5.js
+++ js2/5.js
@@ -6,7 +6,11 @@
  * @param {number} time
  */
 
-const solution = (arr, time) => {
+const solution = (arr, time, i = 0) => {
+  if (i === arr.length) return
+  setTimeout(arr[i], time)
+
+  return solution(arr, time, i + 1)
 }
 
 module.exports = {
